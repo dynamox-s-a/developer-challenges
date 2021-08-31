e77b0abdbc68027d563a4c6a53923261026f4523
Andersson Brito <andersson.brito@wattei.com.br>
2021-08-31 00:45:02 -0300

    Desafio 01 Pronto

---
+++ desafio-01/src/components/Navbar.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from "@material-ui/core/styles";
+import logodynamox from '../assets/img/logodynamox.png';
+import { IconButton, Link, Menu, MenuItem } from '@material-ui/core';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+    },
+    logo: {
+        padding: 3,
+        marginLeft: 30,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    navbar: {
+        background: "#263252",
+
+    },
+    links: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex'
+        },
+
+
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    navButton: {
+        '&:hover': {
+            backgroundColor: '#546492',
+            borderColor: '#546492',
+            boxShadow: 'none',
+        },
+        fontFamily: 'Raleway',
+        fontStyle: 'normal',
+        fontWeight: 500,
+        fontSize: '18px',
+        lineHeight: '23px',
+        color: ' #FFFFFF',
+    }
+
+
+}));
+
+export default function Navbar() {
+    const classes = useStyles();
+
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton color="inherit">
+                    DynaPredict
+                </IconButton>
+            </MenuItem>
+            <MenuItem>
+                <IconButton color="inherit">
+                    Sensores
+                </IconButton>
+            </MenuItem>
+            <MenuItem>
+                <IconButton color="inherit">
+                    Contato
+                </IconButton>
+            </MenuItem>
+        </Menu>
+
+    );
+
+    return (
+        <React.Fragment>
+            <div className={classes.grow}>
+                <AppBar className={classes.navbar}>
+                    <Toolbar>
+                        <Link href={'https://dynamox.net/'}>
+                            <img
+                                className={classes.logo}
+                                alt={"Logo "}
+                                width={172}
+                                src={logodynamox}                               
+                            />
+                        </Link>
+                        <div className={classes.grow} />
+                        <div className={classes.links}>
+                            <IconButton className={classes.navButton} color="inherit" href={' https://dynamox.net/dynapredict/'}>
+                                DynaPredict
+                            </IconButton>
+                            <IconButton className={classes.navButton} color="inherit"  href={'#sensores'}>
+                                Sensores
+                            </IconButton>
+                            <IconButton className={classes.navButton} color="inherit"  href={'#footer'}>
+                                Contato
+                            </IconButton>
+                        </div>
+                        <div className={classes.sectionMobile}>
+                            <IconButton
+
+                                onClick={handleMobileMenuOpen}
+                                color="inherit"
+                            >
+                                <MoreIcon />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                {renderMobileMenu}
+                {renderMenu}
+            </div>
+            <Toolbar />
+
+
+        </React.Fragment>
+    );
+}
--- desafio-01/src/index.js
+++ desafio-01/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './views/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
