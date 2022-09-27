12a7e875255fd893c07a720d2af1ca483e0c26d1
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-27 18:00:59 -0300

    feat: pages

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,7 @@
+import './Form.css'
+
+export function Form() {
+  return (
+    <div className="Form"><h2>teste form</h2></div>
+  );
+};
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+import grafic from '../../assets/images/grafismo.png'
+
+export function Home() {
+  return (
+    <section>
+      <section className="initial">
+
+        <h1>teste</h1>
+      </section>
+      <section className="description">
+        <h2>Sensores para Manutenção Preditiva</h2>
+        <h3>Opções de sensores sem fio, ou DynaLoggers com sensores de vibração triaxial e temperatura embarcados, que comunicam por Bluetooth com o App mobile ou Gateway, registrando os dados monitorados em sua memória interna. Por conexão internet esses dados são centralizados na Plataforma DynaPredict Web para análise, prognóstico e tomada de decisão.</h3>
+
+      </section>
+    </section>
+  );
+};
