63a85ac450b6e481e96f3398748512b570ed53e3
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-27 14:59:33 -0300

    First-commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Home } from './pages/Home/Home'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
