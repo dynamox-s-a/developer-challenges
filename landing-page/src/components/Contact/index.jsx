a8ae22c681f6f8fac99c2607831410ac9f2115d7
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-09-28 18:21:05 -0300

    form interactions

--- landing-page/src/components/Contact/index.jsx
+++ landing-page/src/components/Contact/index.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react';
+import { Modal } from '../Modal';
+
 import './styles.css';
 
 export function Contact() {
+  const [form, setForm] = useState({
+    nome: '',
+    company: '',
+    email: '',
+    phone: ''
+  });
+
+  function handleForm() {
+    const preenchidos = Object.keys(form).filter((key) => {
+      return form[key] !== '';
+    })
+    
+    if(preenchidos.length === 4) {
+      document.querySelector('.modal-overlay').classList.add('active');
+    } else {
+      alert('Preencha todos os campos para entrarmos em contato com você');
+    }
+  }
+
   return(
     <section className="grid-pattern contact" id="contact">
       <div className="contact-section">
@@ -12,14 +34,55 @@ export function Contact() {
           </h3>
 
           <div className="contact-form">
-            <input type="text" placeholder="Como gostaria de ser chamado?" />
-            <input type="text" placeholder="Em qual empresa você trabalha?" />
-            <input type="email" placeholder="Digite aqui o seu email" />
-            <input type="text" placeholder="Qual o seu telefone?" />
-            <button>Enviar</button>
+            <label htmlFor="nome">Nome:</label>
+            <input 
+              id="nome"
+              type="text" 
+              placeholder="Como gostaria de ser chamado?"
+              value={form.name} 
+              onChange={e => setForm(prevent => {
+                return {...prevent, nome: e.target.value}
+              })} 
+            />
+
+            <label htmlFor="company">Empresa:</label>
+            <input 
+              id="company"
+              type="text" 
+              placeholder="Em qual empresa você trabalha?"
+              value={form.company} 
+              onChange={e => setForm(prevent => {
+                return {...prevent, company: e.target.value}
+              })} 
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input 
+              id="email"
+              type="email" 
+              placeholder="Digite aqui o seu email"
+              value={form.email} 
+              onChange={e => setForm(prevent => {
+                return {...prevent, email: e.target.value}
+              })} 
+             />
+
+            <label htmlFor="phone">Telefone:</label>
+            <input 
+              id="phone"
+              type="text" 
+              placeholder="Qual o seu telefone?" 
+              value={form.phone} 
+              onChange={e => setForm(prevent => {
+                return {...prevent, phone: e.target.value}
+              })} 
+            />
+            <button onClick={handleForm}>Enviar</button>
           </div>
         </div>
         
+        <Modal form={form} />
+    
       </div>
     </section>
   )
