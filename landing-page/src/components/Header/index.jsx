4bf32ba8eaeaed6b800e2fdd361754b4a026eb92
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-09-28 10:18:50 -0300

    fixed menu and go to section on click

--- landing-page/src/components/Header/index.jsx
+++ landing-page/src/components/Header/index.jsx
@@ -2,6 +2,15 @@ import logo from '../../assets/logo-dynamox.png';
 import './styles.css';
 
 export function Header() {
+  function scrollToSection(hash) {
+    const section = document.querySelector(hash).offsetTop;
+    console.log(section)
+    window.scroll({
+      top: section - 120,
+      behavior: "smooth"
+    })
+  }
+
   return(
     <header className="grid-pattern">
       <div className="contain-header">
@@ -10,21 +19,19 @@ export function Header() {
           <img src={logo} alt="logo" className="logo" />
         </a>
         
-        <div className="navbar">
-          <nav>
-            <ul>
-              <a href="https://dynamox.net/dynapredict/">
-                <li>DynaPredict</li>
-              </a>
-              <a href="#">
-                <li>Sensores</li>
-              </a>
-              <a href="#">
-                <li>Contato</li>
-              </a>
-            </ul>
-          </nav>
-        </div>
+        <nav className="menu">
+          <ul>
+            <a href="https://dynamox.net/dynapredict/">
+              <li>DynaPredict</li>
+            </a>
+            <a href="#sensores" onClick={() => scrollToSection("#sensors")}>
+              <li>Sensores</li>
+            </a>
+            <a href="#contato" onClick={() => scrollToSection("#contact")}>
+              <li>Contato</li>
+            </a>
+          </ul>
+        </nav>
 
       </div>
     </header>
