9a12a6ef47b6e2c783d4a4e67f7e94ab44d34eae
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-29 14:35:26 -0300

    feat create initial summary

---
+++ client/src/components/Summary/Summary.jsx
@@ -0,0 +1,18 @@
+import './Summary.css'
+
+export function Summary() {
+  return (
+    <div className='summary'>
+      <p>
+        <strong>teste: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo accusantium molestiae quas error sunt eius!
+      </p>
+      <p>
+        <strong>Editar : </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo accusantium molestiae quas error sunt eius!
+      </p>
+
+      <p>
+        <strong>Deletar : </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo accusantium molestiae quas error sunt eius!
+      </p>
+    </div>
+  )
+}
