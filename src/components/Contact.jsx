d1d033713900b2da19f1db5bd285606429c22af2
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-29 00:40:41 -0300

    ✨ feat(): criado e estruturado html do novo componente Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section>
+      <h4>Ficou com dúvida? Nós entramos em contato com você</h4>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">ENVIAR</button>
+      </form>
+    </section>
+  );
+}
