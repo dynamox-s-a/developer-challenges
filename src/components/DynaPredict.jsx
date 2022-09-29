9dfc84dc5f4726e6425aa01505374eafd24d6830
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-29 00:28:31 -0300

    ✨ feat(): estruturado html do componente DynaPredict

--- src/components/DynaPredict.jsx
+++ src/components/DynaPredict.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import LogoSensor from "./LogoSensor";
+import LogoDynapredict from "../assets/logo-dynapredict.png";
+import DesktopAndMobile from "../assets/desktop-and-mobile.png";
 
 export default function DynaPredict() {
   return (
     <section>
       <div>
         <h1>Solução DynaPredict</h1>
-        <div>
-          <img src="" alt="" />
-          <LogoSensor />
-        </div>
-        <div>
-          <img src="" alt="" />
-          <LogoSensor />
-        </div>
+        <img src={LogoDynapredict} alt="logo-dynapredict.png" />
+      </div>
+      <div>
+        <img src={DesktopAndMobile} alt="desktop-and-mobile.png" />
       </div>
     </section>
   );
