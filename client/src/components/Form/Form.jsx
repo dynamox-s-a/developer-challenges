84a57dfa5fdc749a819768a091a0fed5f6535a71
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-29 19:46:13 -0300

    feat: route create

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,11 +1,92 @@
-import './Form.css'
+import { useState } from "react";
+import { api } from "../../utils/Api/Api";
+import "./Form.css"
 
 export function Form() {
+  const [newProduct, setNewProduct] = useState();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setNewProduct({ ...newProduct });
+    var qtdForm = 0;
+    var key
+    for (key in newProduct) {
+      if (newProduct.hasOwnProperty(key)) {
+        qtdForm++;
+      }
+    }
+
+    if (newProduct.nome == "" || newProduct.dataFabricacao == "" || qtdForm < 5) {
+      alert("Preencha todos os campos")
+    } else {
+      console.log(newProduct)
+      await api.createProduct(newProduct);
+      // location.reload();
+    }
+  }
 
   return (
-
     <div className="form">
+      <form onSubmit={handleSubmit} className="form-inputs">
+        <section>
+          <span>Nome: </span>
+          <input className='form-nome'
+            type="text"
+            name="nome"
+            onChange={(event) => {
+              setNewProduct({ ...newProduct, nome: event.target.value });
+            }}
+          ></input>
+        </section>
+        <section>
+          <span>Data de fabricação: </span>
+          <input className='form-dataFabricacao'
+            type="text"
+            name="dataFabricacao"
+            onChange={(event) => {
+              setNewProduct({ ...newProduct, dataFabricacao: event.target.value });
+            }}
+          ></input>
 
+        </section><br />
+        <section>
+          <span>Produto perecivel: </span>
+          <input className='form-perecivel'
+            type="text"
+            name="perecivel"
+            onChange={(event) => {
+              setNewProduct({ ...newProduct, perecivel: event.target.value });
+            }}
+          ></input>
+
+        </section><br />
+        <section>
+          <span>Data de validade: </span>
+          <input className='form-dataValidade'
+            type="text"
+            name="dataValidade"
+            onChange={(event) => {
+              setNewProduct({ ...newProduct, dataValidade: event.target.value });
+            }}
+          ></input>
+
+        </section><br />
+        <section>
+          <span>Preço: R$</span>
+          <input className='form-preco'
+            type="text"
+            name="preco"
+            onChange={(event) => {
+              setNewProduct({ ...newProduct, preco: event.target.value });
+            }}
+          ></input>
+
+        </section><br />
+        <button type="submit" className="btn-submit">
+          ADICIONAR
+        </button>
+      </form>
     </div>
   );
-};
+}
