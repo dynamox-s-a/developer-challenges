419853d1a5203b84445166599bae6e56cd037bfc
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-09-30 00:27:23 -0300

    style: adjust some css properties

--- desafio-01/src/components/SolutionSection.jsx
+++ desafio-01/src/components/SolutionSection.jsx
@@ -6,15 +6,13 @@ import desktopAndMobile from '../images/desktop-and-mobile.png';
 function SolutionSection() {
   return (
     <section className='flex bg-cover bg-no-repeat bg-top justify-center h-screen' style={ { backgroundImage: `url(${bgImage})`}}>
-      <div className='md:flex md:shrink-0'>
-      <div className='flex flex-col justify-center ml-10 mr-20 text-white font-raleway font-bold 2xl:text-8xl lg:text-6xl sm:text-4xl '>
+      <div className='flex flex-col justify-center mb-40 ml-10 mr-20 text-white font-raleway font-bold 2xl:text-8xl lg:text-6xl sm:text-4xl '>
         <h1 className='mb-3'>Solução</h1>
         <hi className='mb-14'>DynaPredict</hi>
-        <img className='w-44 h-8' src={logoDynaPredict} alt="Logo-DynaPredict" />
+        <img className='w-44 h-8 mb-28' src={logoDynaPredict} alt="Logo-DynaPredict" />
       </div>
-      <div className=''>
+      <div>
         <img className='xl:h-laptop-image xl:w-laptop-image lg:h-4/5 lg:w-full md:h-4/5 md:w-4/5 'src={desktopAndMobile} alt="Desktop and mobile" />
-      </div>
       </div>
     </section>
   );
