3ac7fefe401ba96f2076cc1dd596dde3893c7e68
Gabriel Mendes <gabrielmendesa@gmail.com>
2023-01-05 19:45:25 -0300

    creating and styling navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient__bg">
+      <div className="initial__bg">
         <Navbar />
         <Header />
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets-desafio-01/logo-dynamox.png';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <a href="https://dynamox.net/"><img src={logo} alt="logo" /></a>
+        </div>
+        <div className="navbar-links_container">
+          <p><a href="https://dynamox.net/dynapredict/">DynaPredict</a></p>
+          <p><a href="#Sensores">Sensores</a></p>
+          <p><a href="#Contato">Contato</a></p>
+        </div>
+      </div>
     </div>
   )
 }
