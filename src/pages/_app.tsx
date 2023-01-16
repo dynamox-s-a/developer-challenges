e986ac97760c49d37605d2bceed81bf8ea1248c2
Matheus <matheus-rodrigues37@live.com>
2023-01-16 14:28:50 -0300

    add prettier and config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Raleway } from "@next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
