06675b944723bc97d088b21cfbda4ba8fc2bb8be
Matheus <matheus-rodrigues37@live.com>
2023-01-16 17:03:57 -0300

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Dynamox</title>
+        <meta name="title" content="Dynamox landing page" />
         <meta
           name="description"
           content="Opções de sensores sem fio, ou DynaLoggers com sensores de vibração triaxial e temperatura embarcados, que comunicam por Bluetooth com o App mobile ou Gateway, registrando os dados monitorados em sua memória interna. Por conexão internet esses dados são centralizados na Plataforma DynaPredict Web para análise, prognóstico e tomada de decisão."
