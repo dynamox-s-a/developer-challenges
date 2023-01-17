d83d1e3c765fb6fd32dfc0b1b078db9bc4a647ab
Matheus <matheus-rodrigues37@live.com>
2023-01-17 16:11:21 -0300

    config app and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,62 @@
-const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+import { useEffect } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { useAppDispatch } from "./app/hooks";
+import { setUser } from "./features/auth.slice";
+import PrivateRoute from "./utils/PrivateRoute";
+import Details from "./pages/Details";
+import New from "./pages/New";
+import Update from "./pages/Update";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  useEffect(() => {
+    dispatch(setUser({ accessToken: user.token, user: { email: user.email } }));
+  }, [dispatch, user]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/details/:id"
+          element={
+            <PrivateRoute>
+              <Details />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/update/:id"
+          element={
+            <PrivateRoute>
+              <Update />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <PrivateRoute>
+              <New />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+
+if (!container) throw new Error("Could not find root element with id 'root'");
+
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <ToastContainer />
     <App />
-  </React.StrictMode>
+  </Provider>
 );
