e15e9926c611c38395e857d121c3798fa59251cf
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-11 19:30:42 -0300

    chore: change method of font implementation and update unecessary code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
-import { Inter } from 'next/font/google'
 import Header from '@component/components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+    </main>
   )
 }
