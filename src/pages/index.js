471fb0cd425ce40af146f6ba12963859d2e27c27
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-11 20:50:16 -0300

    feat: add SolutionSection component in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Header from '@component/components/Header'
+import SolutionSection from '@component/components/SolutionSection'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <SolutionSection />
     </main>
   )
 }
