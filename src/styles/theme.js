8ace6a4cba0aff58963d3f6e15658449f848da6a
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-11 19:08:42 -0300

    feat: update global.css and implement theme.js for creating theme for MUI/Material

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#263252',
+      light: '#F4F7FC'
+    },
+  },
+  typography: {
+    fontFamily: 'Raleway, sans-serif',
+    fontSize: 20
+  },
+})
