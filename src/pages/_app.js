d91eff2441e8efffc94eb9723560b98fa7f1149b
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-11 19:11:03 -0300

    feat: implement ThemeProvider in App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@component/styles/globals.css'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '@component/styles/theme'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
