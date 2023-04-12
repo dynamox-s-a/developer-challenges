0471536c1fe2990de60070a47501bec7754d13ef
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-12 00:10:55 -0300

    feat: add SensorsSection component in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Header from '@component/components/Header'
 import SolutionSection from '@component/components/SolutionSection'
+import SensorsSection from '@component/components/SensorsSection'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <SolutionSection />
+      <SensorsSection />
     </main>
   )
 }
