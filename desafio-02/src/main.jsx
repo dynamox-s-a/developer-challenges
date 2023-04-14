4cc539c2e7a7c9903d1332e98c85d69c78aafe45
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-14 16:33:16 -0300

    feat: implement Provider and redux-store

--- desafio-02/src/main.jsx
+++ desafio-02/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-// import store from './store'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
 )
