a5cfc4fd6eb0e6ee5378efd63e0f418445832e8b
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-14 16:36:42 -0300

    feat: implement route structure with the required routes/elements

--- desafio-02/src/App.jsx
+++ desafio-02/src/App.jsx
@@ -1,21 +1,27 @@
+import { useSelector } from 'react-redux'
 import { Routes, Route, Navigate, Outlet  } from 'react-router-dom'
-import Login from './'
+import AddProducts from './pages/AddProducts'
+import DeleteProduct from './pages/DeleteProduct'
+import EditProduct from './pages/EditProduct'
+import Login from './pages/Login'
+import Products from './pages/Products'
+import UserDashboard from './pages/UserDashboard'
 import './styles/App.css'
 
 function PrivateRoute() {
-  const { isAuth } = useAppSelector((state) => state.auth)
-  if (!isAuth) return <Navigate to="/login" replace />
+  const { userInfo } = useSelector((state) => state.auth)
+  if (!userInfo) return <Navigate to="/" replace />
   return <Outlet />
 }
 
 function App() {
   return (
     <Routes>
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Login />} />
       <Route path="/user" element={<PrivateRoute />}>
         <Route path="/user" element={ <UserDashboard /> }>
           <Route path="/user/productsList" element={ <Products /> } />
-          <Route path="/user/addProduct" element={ <AddProduct /> } />
+          <Route path="/user/addProduct" element={ <AddProducts /> } />
           <Route path="/user/editProduct" element={ <EditProduct /> } />
           <Route path="/user/removeProduct" element={ <DeleteProduct /> } />
         </Route>
