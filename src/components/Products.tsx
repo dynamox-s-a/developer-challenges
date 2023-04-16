4cfb8850d5bc4a1a6a3336618896e98031cdf7a0
Giovanna Trigueiro <giovanna.trigueiro@gmail.com>
2023-04-16 14:16:00 -0300

    feat: pagination

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,10 +3,27 @@ import { useProducts } from '@/hooks/useProducts';
 import CreateProduct from '@/components/CreateProduct';
 import { useMutation, useQueryClient } from 'react-query';
 import { api } from '@/pages/api/hello';
+import { useState } from 'react';
 
 const Products: React.FC = () => {
 	const queryClient = useQueryClient();
 	const { data, isLoading, error } = useProducts();
+
+	const [pageSize, setPageSize] = useState(10);
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const startIndex = (currentPage - 1) * pageSize;
+	const productsToShow = data.slice(startIndex, startIndex + pageSize);
+
+	const pageCount = Math.ceil(data.length / pageSize);
+
+	function goToPreviousPage() {
+		setCurrentPage(currentPage - 1);
+	}
+
+	function goToNextPage() {
+		setCurrentPage(currentPage + 1);
+	}
 
 	const deleteProduct = useMutation(
 		(id: number) => api.delete(`/products/${id}`),
@@ -29,7 +46,7 @@ const Products: React.FC = () => {
 		return <div>Error</div>;
 	}
 	return (
-		<div className='flex flex-col h-screen w-screen bg-slate-900 text-white'>
+		<div className='flex flex-col h-fit w-screen bg-slate-900 text-white'>
 			<Dialog.Root>
 				<Dialog.Trigger className='bg-pink-700 hover:bg-pink-800 w-fit h-fit p-3'>
 					Criar Novo Produto
@@ -42,7 +59,7 @@ const Products: React.FC = () => {
 			<section>
 				<h1 className='font-bold text-xl'>Meus Produtos</h1>
 				<div>
-					{data.map((product: any) => {
+					{productsToShow.map((product: any) => {
 						return (
 							<div key={product.id}>
 								<div className='rounded-sm hover:bg-pink-900'>
@@ -69,6 +86,14 @@ const Products: React.FC = () => {
 							</div>
 						);
 					})}
+					<div>
+						{currentPage > 1 && (
+							<button onClick={goToPreviousPage}>Previous Page</button>
+						)}
+						{currentPage < pageCount && (
+							<button onClick={goToNextPage}>Next Page</button>
+						)}
+					</div>
 				</div>
 			</section>
 		</div>
