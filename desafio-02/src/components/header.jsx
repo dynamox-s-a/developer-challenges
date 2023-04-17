e607f7a9e4e6d85b8eeda6d1355af928e5c1149a
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-04-17 15:29:21 -0300

    feat: add fetchProduct to link 'Lista'

--- desafio-02/src/components/header.jsx
+++ desafio-02/src/components/header.jsx
@@ -11,10 +11,17 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../redux/auth/authSlice";
+import "./header.css";
+import { fetchProducts } from "../redux/products/productsActions";
 
 export const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+
+  const handleFetchProducts = () => {
+    dispatch(fetchProducts());
+  };
 
   const handleLogout = () => {
     dispatch(logout());
@@ -22,23 +29,25 @@ export const Header = () => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" className="appBar">
       <Toolbar>
-        <Box>
-          <Typography variant="h6" component="div">
-            <Link to="/user" component={MuiLink}>
+        <Box className="titleContainer">
+          <Typography variant="h6" component="div" className="title">
+            <Link to="/user" onClick={() => handleFetchProducts()} component={MuiLink} className="link">
               Lista
             </Link>
           </Typography>
-          <Typography>
-            <Link to="/user/addProduct" component={MuiLink} color="inherit">
+          <Typography variant="h6" component="div" className="title">
+            <Link to="/user/addProduct" component={MuiLink} className="link">
               Adicionar produto
             </Link>
           </Typography>
         </Box>
-        <Button color="inherit" onClick={handleLogout}>
-          Logout
-        </Button>
+        <Box className="button-container">
+          <Button color="inherit" onClick={handleLogout} className="button">
+            Logout
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
