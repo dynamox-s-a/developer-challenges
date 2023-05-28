92bf1786fde279cd6a792ad34144f091b87e0788
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-05-28 18:50:50 -0300

    feat: add find by id

---
+++ pages/api/machine/read/[...id].page.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "prisma/client";
+import IsSigned from "lib/auth/is-signed";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isSigned = await IsSigned(req, res);
+  if (!isSigned) {
+    res.status(401).json(req.body);
+  } else {
+    if (Array.isArray(req.query.id)) req.query.id = req.query.id[0];
+    const id: number = parseInt(req.query.id || "");
+
+    const machine = await prisma.machine.findUnique({
+      where: { id },
+    });
+
+    res.status(200).json(machine);
+  }
+}
