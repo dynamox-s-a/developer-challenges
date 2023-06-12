660bbed487a20bbc9ecc3f4423ae5b21eced5283
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-06-12 11:27:04 -0300

    added material ui

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6f2746830ac7c004"; }
+/******/ 	__webpack_require__.h = function() { return "c04427f90c3079b2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
