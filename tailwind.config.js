0a6f58b780140e0149ebfa70ab6a40c22fa76c5e
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-06-15 13:30:04 -0300

    replaced purge with content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
+  content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
