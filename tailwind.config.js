c8e3b27ddc3716b65da4eaaca529b7227f77ae16
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-06-15 13:36:18 -0300

    changed tailwind config to tsx only

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.tsx",
+    "./src/pages/**/*.tsx",
+    "./src/components/**/*.tsx"
   ],
   theme: {
     extend: {},
