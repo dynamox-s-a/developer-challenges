4503a814c09d8ed05dc69e3365acbeab1cb837d1
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-06-15 13:18:59 -0300

    added purge to tailwind config and specified directories further

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
+  purge: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
