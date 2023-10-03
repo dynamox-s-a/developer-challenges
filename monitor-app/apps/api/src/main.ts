c2bb5426efea24d8a7913aca7fe9f865f67ad54e
JP <pagno.jp@gmail.com>
2023-10-03 19:23:28 -0300

    feat(api): add cors

--- monitor-app/apps/api/src/main.ts
+++ monitor-app/apps/api/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   )
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
+  app.enableCors()
   const port = process.env.PORT || 3000
   await app.listen(port)
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
