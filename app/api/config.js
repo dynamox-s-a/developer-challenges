8277e4f4af497acc64a2d7848773d2d223de974d
francis.targanski <francis.targanski@2mi.com.br>
2023-10-24 10:43:30 -0300

    feature (db) deploy data

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "http://localhost:3001";
 
-const API_BASE_URL = "https://json-server-wine-eight.vercel.app/db";
+const API_BASE_URL = "https://json-server-wine-eight.vercel.app";
 
 
 export default API_BASE_URL;
