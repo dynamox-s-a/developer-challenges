dc8d5490b4f82fb9e12e4e45e699f11baaba66de
francis.targanski <francis.targanski@2mi.com.br>
2023-10-23 23:13:34 -0300

    feature (deploy) api rest

--- app/api/config.js
+++ app/api/config.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = "http://localhost:3001";
+// const API_BASE_URL = "http://localhost:3001";
+
+const API_BASE_URL = "https://test-dynamox.vercel.app/";
+
 
 export default API_BASE_URL;
