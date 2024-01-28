990cda6e54b4270bae9ce7352656a30b01dc118e
Gustavo Guimarães Silva <chdrlz@gmail.com>
2024-01-27 22:45:15 -0300

    feat: implement the GET and the POST methods of /api/machine endpoint

---
+++ app/api/machine/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { postSchema } from "./schema";
+
+export async function GET(request: NextRequest) {
+  const machines = await prisma.machine.findMany();
+
+  return NextResponse.json(machines);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = postSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
+  }
+
+  const validatedBody = validation.data;
+
+  const machine = await prisma.machine.create({
+    data: {
+      name: validatedBody.name,
+      type: validatedBody.type,
+      userId: validatedBody.userId,
+    },
+  });
+
+  return NextResponse.json(machine);
+}
---
+++ app/api/machine/schema.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+function typeValidation(type: string) {
+  const allowedTypes = ["fan", "pump"];
+  let typeIsValidated = false;
+
+  allowedTypes.map((allowedType) => {
+    if (type === allowedType) typeIsValidated = true;
+  });
+
+  if (!typeIsValidated) return false;
+  return type;
+}
+
+const postSchema = z.object({
+  name: z.string().min(3),
+  type: z.string().toLowerCase().refine(typeValidation, {
+    message: "Invalid machine type. Machine type needs to be fan or pump.",
+  }),
+  userId: z.number(),
+});
+
+export { postSchema };
+
+const putSchema = z.object({
+  name: z.string().min(3),
+  type: z.string().toLowerCase().refine(typeValidation, {
+    message: "Invalid machine type. Machine type needs to be fan or pump.",
+  }),
+});
+
+export { putSchema };
