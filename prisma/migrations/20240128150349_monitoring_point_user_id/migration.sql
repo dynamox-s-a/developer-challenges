2ff1b6d00766daa81b8e64d4e93dc06b362cd6e6
Gustavo Guimarães Silva <chdrlz@gmail.com>
2024-02-02 00:11:27 -0300

    del: delete all migrations

--- prisma/migrations/20240128150349_monitoring_point_user_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "MonitoringPoint" ADD COLUMN     "userId" INTEGER NOT NULL DEFAULT 1;
-
--- AddForeignKey
-ALTER TABLE "MonitoringPoint" ADD CONSTRAINT "MonitoringPoint_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20240128150532_monitoring_point_adjust_user_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "MonitoringPoint" ALTER COLUMN "userId" DROP DEFAULT;
