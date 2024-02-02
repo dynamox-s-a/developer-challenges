4809a319f13e201d5d581d7821bc179055bfbf2a
Davi Raquel <daviraquel@gmail.com>
2024-02-02 10:29:02 -0300

    fix sensor image
    now image changes when selected sensor changes

--- davi-raquel/frontend-challenge/src/components/SensorInfo.tsx
+++ davi-raquel/frontend-challenge/src/components/SensorInfo.tsx
@@ -4,11 +4,14 @@ import Box from "@mui/material/Box"
 import { useAppSelector } from "../app/hooks"
 import { selectSensor } from "../features/sensors/sensorSlice"
 
-import sensorPic from "../assets/img/sensor-af.png"
+import sensorPic1 from "../assets/img/sensor-af.png"
+import sensorPic2 from "../assets/img/sensor-hf.png"
+import sensorPic3 from "../assets/img/sensor-tca.png"
 
 export const SensorInfo = () => {
   const sensor = useAppSelector(selectSensor)
-
+  const imgUrl =
+    sensor.id === 3 ? sensorPic3 : sensor.id === 2 ? sensorPic2 : sensorPic1
   return (
     <Box sx={{ display: "flex" }}>
       <Box
@@ -17,9 +20,13 @@ export const SensorInfo = () => {
           objectFit: "cover",
           maxHeight: { xs: 100, md: 150 },
           maxWidth: { xs: 100, md: 150 },
+          content: {
+            xs: "none", //img src from xs up to md
+            md: `url(${imgUrl})`, //img src from md and up
+          },
         }}
         alt={`picture of ${sensor}`}
-        src={sensorPic}
+        // src={sensorPic}
       />
       <Box sx={{ color: "white" }}>
         <Typography component="h4">Title: {sensor.title}</Typography>
