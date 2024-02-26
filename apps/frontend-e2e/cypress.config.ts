b66e58ef1cc07aebc0a91c9ac7679445bdfdd109
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-26 11:33:19 -0300

    create nx workspace with nestjs and reactjs

---
+++ apps/frontend-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run frontend:serve',
+        production: 'nx run frontend:preview',
+      },
+      ciWebServerCommand: 'nx run frontend:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
