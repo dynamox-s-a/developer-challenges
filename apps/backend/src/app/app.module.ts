75e89dca138c0aa1de123731e6092c0cdf07338c
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-28 12:31:32 -0300

    build: remove react frontend to deploy backend on render

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,29 +1,13 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { SensorsModule } from './sensors/sensors.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { MachinesModule } from './machines/machines.module';
 import { MonitoringPointsModule } from './monitoring-points/monitoring-points.module';
 
-// const rootPath =
-//   process.env.NODE_ENV === 'development'
-//     ? join(__dirname, '../../../apps/frontend/dist/')
-//     : join(__dirname, '../../../frontend/dist/');
-
-const rootPath =
-  process.env.NODE_ENV === 'development'
-    ? join(__dirname, '../../../dist/apps/frontend/')
-    : join(process.cwd(), 'apps/frontend/dist/');
-
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: rootPath,
-      exclude: ['api/*'],
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -16,11 +16,9 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
