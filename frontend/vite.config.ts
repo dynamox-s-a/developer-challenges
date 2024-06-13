eb3bfb4ad4e2068f59a0d899594863bbc5f349ab
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2024-06-12 22:15:47 -0300

    feat: FE docker start

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: false,
+    port: 3000,
+    host: true,
+    origin: 'http://0.0.0.0:3000',
+  },
+  preview: {
+    open: false,
+    port: 3000,
+    host: true,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests',
     mockReset: true,
   },
-})
+});
