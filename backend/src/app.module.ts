ae8abd798cd23b0945bae2c42e1a992cf0b7e4cb
Gabriel-Arins <gabriel.arins@gmail.com>
2024-06-20 17:45:50 -0300

    chore: Update npm dependencies to latest versions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
