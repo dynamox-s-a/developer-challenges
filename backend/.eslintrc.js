2336861ef2d215faad0ed47f8cbdca83bd62125c
Gabriel-Arins <gabriel.arins@gmail.com>
2024-06-20 17:26:55 -0300

    chore: Remove unused code files

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    '@rocketseat/eslint-config/node'
   ],
   root: true,
   env: {
@@ -21,5 +22,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": [
+    "error",
+    {
+      "endOfLine": "auto"
+    },
+  ],
+  'no-useless-constructor': 'off',
+  
   },
 };
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
