0980bc66489e83ca315002d2647e59218dbe3867
Gabriel-Arins <gabriel.arins@gmail.com>
2024-06-23 10:56:04 -0300

    feat(auth): create files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [PrismaModule, UserModule],
+	imports: [PrismaModule, UserModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
