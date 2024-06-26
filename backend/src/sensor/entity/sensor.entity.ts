a0c8c3a08a506b4dff23db35e87341b7fff491b5
Gabriel-Arins <gabriel.arins@gmail.com>
2024-06-25 22:12:06 -0300

    chore: fixed sensor entity

--- backend/src/sensor/entity/sensor.entity.ts
+++ backend/src/sensor/entity/sensor.entity.ts
@@ -1,6 +1,8 @@
 import { Prisma } from '@prisma/client'
 
 export class Sensor implements Prisma.SensorCreateInput {
+    sensor_id: number
+    monitoring_point_id: number 
     machine_id: number
     monitoring_point?: string
     sensor_type: string
