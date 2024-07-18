9764af71d6e2894ed2a2a630d164df0b0b7ef748
ccarol-freitas <carolfreitas101996@gmail.com>
2024-07-18 13:36:22 -0300

    chore: rollback test

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
-  
 })
 
