f81fc1f5fbc7f057ae27a5cc067128f8310992e1
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2024-10-26 12:40:33 -0300

    created redux folder

---
+++ full-stack-challenge/features/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
