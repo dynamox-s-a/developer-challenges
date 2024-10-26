87069e597877b564ec3e12f694b455b720182e78
Lucas de Aguiar <lucasaguiaresteves@outlook.com>
2024-10-26 12:42:47 -0300

    implemented authentication

--- full-stack-challenge/features/store.ts
+++ full-stack-challenge/features/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
+import authReducer from "./authSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
