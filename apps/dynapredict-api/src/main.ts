6b5b03e3df899acdfdbe484e7a69c5f93ad4db20
João Vítor Ferraz <git@p1pa.dev>
2024-10-27 11:41:25 -0300

    feat: forbid unknown payloads to routes

--- apps/dynapredict-api/src/main.ts
+++ apps/dynapredict-api/src/main.ts
@@ -7,7 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
