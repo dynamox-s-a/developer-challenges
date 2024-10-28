a37696af7e1dd0f3a0784c4381418ccba9a6e671
mateusbays <mateusbays@hotmail.com>
2024-10-28 00:10:31 -0300

    fix: change healcheck endpoint to be a public route

--- mateus-bays/dynamox-auth/src/app/app.controller.ts
+++ mateus-bays/dynamox-auth/src/app/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-
 import { AppService } from './app.service';
+import { Public } from 'modules/decorators/setMetadata.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getData() {
     return this.appService.getData();
