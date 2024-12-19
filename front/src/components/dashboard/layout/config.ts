4717ee8a9d0e664863c869137cfdd44dab067389
Gabriela Schlemper da Silva <gabschlemper@gmail.com>
2024-12-18 21:20:17 -0300

    remove useless files

--- front/src/components/dashboard/layout/config.ts
+++
@@ -1,9 +0,0 @@
-import type { NavItemConfig } from '@/types/nav';
-import { paths } from '@/paths';
-
-export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'machines', title: 'Machines', href: paths.dashboard.machines, icon: 'chart-pie' },
-  // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  // { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
-] satisfies NavItemConfig[];
