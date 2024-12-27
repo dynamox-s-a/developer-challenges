6e2a557f9c96cb1cfb41e01e0b1baae5d00decad
Gabriela Schlemper <schhlemper@gmail.com>
2024-12-27 20:09:07 -0300

    extend session types for express

--- backend/types/session.d.ts
+++ backend/types/session.d.ts
@@ -1,7 +1,14 @@
 import { User } from '@prisma/client';
+import 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
     user: User;
+  }
+}
+
+declare module 'express' {
+  interface Request {
+    session: Session & Partial<SessionData>;
   }
 }
