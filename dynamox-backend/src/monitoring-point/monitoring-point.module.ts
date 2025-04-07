0b3797ef85715bc7af3ed9f0aef96fdb8560d9fa
Karina <karinaperesg@gmail.com>
2025-04-07 19:18:13 -0300

    feat(backend): monitorinf-point.module.ts

--- dynamox-backend/src/monitoring-point/monitoring-point.module.ts
+++ dynamox-backend/src/monitoring-point/monitoring-point.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MonitoringPointService } from './monitoring-point.service';
 import { MonitoringPointController } from './monitoring-point.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MonitoringPointController],
   providers: [MonitoringPointService],
 })
