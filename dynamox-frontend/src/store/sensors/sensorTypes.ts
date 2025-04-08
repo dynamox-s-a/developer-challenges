188ca24f1a51218f089397980cf66150c3228641
Karina <karinaperesg@gmail.com>
2025-04-08 08:56:41 -0300

    feat(frontend): interface Machine e MonitoringPoint

--- dynamox-frontend/src/store/sensors/sensorTypes.ts
+++ dynamox-frontend/src/store/sensors/sensorTypes.ts
@@ -1,7 +1,23 @@
+export interface Machine {
+  id: number;
+  name: string;
+  type: string;
+}
+
+export interface MonitoringPoint {
+  id: number;
+  name: string;
+  machine: Machine;
+}
+
 export interface Sensor {
   id: number;
   name: string;
   model: "TcAg" | "TcAs" | "HF_Plus";
-  machineId: number;
   monitoringPointId: number;
+  monitoringPoint: MonitoringPoint;
+  machineId: number;
 }
+
+export type CreateSensorDTO = Omit<Sensor, "id" | "monitoringPoint">;
+export type UpdateSensorDTO = Partial<CreateSensorDTO> & { id: number };
