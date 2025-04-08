f05a8a52e35dcbb8f0ac00c37a463523c77db4fa
Karina <karinaperesg@gmail.com>
2025-04-08 19:38:23 -0300

    fix(backend): ajustes para exclusão da rota sensors

--- dynamox-backend/src/app.module.ts
+++ dynamox-backend/src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MachinesModule } from './machines/machines.module';
-import { SensorsModule } from './sensors/sensors.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MonitoringPointModule } from './monitoring-points/monitoring-points.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    MachinesModule,
-    SensorsModule,
-    PrismaModule,
-    MonitoringPointModule,
-  ],
+  imports: [AuthModule, MachinesModule, PrismaModule, MonitoringPointModule],
 })
 export class AppModule {}
