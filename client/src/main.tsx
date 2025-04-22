ad3f725f79e7a560cb132eb557580ed5be27369e
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-04-22 18:22:44 -0300

    Ajustes deploy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,8 @@ import { AuthProvider } from './context/AuthContext';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
---
+++ client/src/react-app-env.d.ts
@@ -0,0 +1,4 @@
+declare module "react-dom/client" {
+    var createRoot: any;
+    export {createRoot};
+}
