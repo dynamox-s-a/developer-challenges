4d8cb00d6e7ea3bf5cde71a8c4100d847e253f75
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-04-22 14:58:41 -0300

    ajustes para deploy

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectToDatabase } from './config/db';
+import authRoutes from './routes/authRoutes';
+import machinesRoutes from './routes/machines';
+import monitoringPointRoutes from './routes/monitoringPoints';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectToDatabase();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/machines', machinesRoutes);
+app.use('/api/monitoring-points', monitoringPointRoutes);
+
+
+const clientBuildPath = path.join(__dirname, '..', '..', 'client', 'build');
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
