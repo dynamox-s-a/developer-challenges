642b2d17801c1700ec30d404357297675c4aec29
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-04-22 20:49:52 -0300

    Ajustes deploy

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -13,7 +13,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   const login = async (email: string, password: string) => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://developer-challenges-507u.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
