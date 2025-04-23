a42ed4a332f62aaa13a6cd418b27d5ba3300079a
albanogabriel <albanogabriel33@gmail.com>
2025-04-23 16:43:09 -0300

    :sparkles: feat: first steps from home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,31 @@
+import { Box } from '@mui/material'
+import { CMSSection } from '../components/section'
+
+import { CMS_DATA_SECTIONS } from '../constants/CMS_DATA'
+
+export function Home() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {CMS_DATA_SECTIONS.map((section, index) => {
+          return <CMSSection data={section} key={section.id} index={index} />
+        })}
+      </Box>
+    </Box>
+  )
+}
