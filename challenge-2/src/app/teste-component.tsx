828acf6663e87d1058d97643c38bbc674ed96733
albanogabriel <albanogabriel33@gmail.com>
2025-04-25 18:17:12 -0300

    :broom: cleanup: remove unused files

--- challenge-2/src/app/teste-component.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { Box, useTheme } from '@mui/material'
-
-export function TesteComponent() {
-  const theme = useTheme()
-  return <Box sx={{ color: theme.palette.primary.main }}>Teste compoenente</Box>
-}
