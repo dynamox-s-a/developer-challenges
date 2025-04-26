9fa3231e7787d57ca8e5d9b5b24173ab79d4ed18
albanogabriel <albanogabriel33@gmail.com>
2025-04-26 08:44:03 -0300

    :wrench: chore: install and setup Axios

---
+++ challenge-2/src/lib/axios.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL
+
+export const clientAxios = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// INTERCEPT REQUEST
+clientAxios.interceptors.request.use(
+  async (config) => {
+    if (config.url === '/login' || config.url === '/signup') {
+      return config
+    }
+
+    const accessToken = localStorage.getItem('token')
+
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`
+      console.log(`Bearer ${accessToken}`)
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// INTERCEPT RESPONSE
+clientAxios.interceptors.response.use((response) => {
+  return response
+})
