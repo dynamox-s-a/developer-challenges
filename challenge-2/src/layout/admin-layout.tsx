4cbbddd096457b6bdc1baf584fe222ced2e29c77
albanogabriel <albanogabriel33@gmail.com>
2025-04-26 20:41:58 -0300

    :sparkles: feat: admin layout

--- challenge-2/src/layout/admin-layout.tsx
+++ challenge-2/src/layout/admin-layout.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch } from '@/store/store'
 import { logout } from '@/store/thunk/auth-thunk'
-import { Button } from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
+import { Power } from 'lucide-react'
 import { ReactNode } from 'react'
 
 interface AdminLayoutProps {
@@ -16,8 +17,35 @@ export function AdminLayout({ children }: AdminLayoutProps) {
 
   return (
     <>
-      Layout de Administrador
-      <Button onClick={logoutApp}>Logout</Button>
+      <Box component={'header'} sx={{ background: '#333333', color: 'white', padding: '16px' }}>
+        <Container
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            fontWeight: '500',
+          }}
+        >
+          Dynamox Admin Dashboard | Eventos
+          <Button
+            onClick={logoutApp}
+            variant="contained"
+            sx={{
+              background: 'white',
+              color: '#333333',
+              display: 'flex',
+              gap: '8px',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography sx={{ fontFamily: 'roboto', fontSize: '14px', alignSelf: 'baseline' }}>
+              Sair
+            </Typography>{' '}
+            <Power size={14} />
+          </Button>
+        </Container>
+      </Box>
       {children}
     </>
   )
