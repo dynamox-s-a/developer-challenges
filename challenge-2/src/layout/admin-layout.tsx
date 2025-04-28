47c39eb7ef56a8452213272f2c043ae5308c82b1
albanogabriel <albanogabriel33@gmail.com>
2025-04-28 08:05:22 -0300

    :iphone: feat: implement menu hamburger on admin and private layout

--- challenge-2/src/layout/admin-layout.tsx
+++ challenge-2/src/layout/admin-layout.tsx
@@ -1,8 +1,10 @@
 import { useAppDispatch } from '@/store/store'
 import { logout } from '@/store/thunk/auth-thunk'
-import { Box, Button, Container, Typography } from '@mui/material'
-import { Power } from 'lucide-react'
-import { ReactNode } from 'react'
+import { Box, Button, Container, IconButton, Menu, MenuItem, Typography } from '@mui/material'
+import { MenuIcon, Power } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode, useState } from 'react'
 
 interface AdminLayoutProps {
   children: ReactNode
@@ -10,9 +12,19 @@ interface AdminLayoutProps {
 
 export function AdminLayout({ children }: AdminLayoutProps) {
   const dispatch = useAppDispatch()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
 
   function logoutApp() {
     dispatch(logout())
+  }
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
   }
 
   return (
@@ -23,36 +35,103 @@ export function AdminLayout({ children }: AdminLayoutProps) {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
+            flexDirection: { xs: 'row-reverse', md: 'row' },
             fontWeight: '500',
           }}
         >
-          Dynamox Admin Dashboard | Eventos
-          <Button
-            onClick={logoutApp}
-            variant="contained"
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Link href={'/'}>
+            <Image src={'/dynamox-logo-white.svg'} alt="Logo Dynamox" width={96} height={40} />
+          </Link>
+
+          <Typography
+            sx={{ display: { xs: 'none', md: 'block' }, fontSize: '16px', fontWeight: 600 }}
+          >
+            Admin Dashboard
+          </Typography>
+
+          {/* DESKTOP */}
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Button
+              onClick={logoutApp}
+              variant="outlined"
+              sx={{
+                // background: 'white',
+                color: 'white',
+                display: 'flex',
+                gap: '8px',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: '1px solid white',
+              }}
+            >
+              <Typography
+                sx={{
+                  fontFamily: 'roboto',
+                  fontSize: '14px',
+                  alignSelf: 'baseline',
+                  textTransform: 'none',
+                }}
+              >
+                Sair
+              </Typography>{' '}
+              <Power size={14} />
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+
+      {/* MOBILE */}
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+        }}
+      >
+        <MenuItem onClick={handleMenuClose}>
+          <Typography sx={{ fontSize: '16px', fontWeight: 600 }}>Eventos da dynamox</Typography>
+        </MenuItem>
+
+        <MenuItem onClick={logoutApp}>
+          <Box
             sx={{
-              background: 'white',
-              color: '#333333',
+              color: '#692746',
               display: 'flex',
               gap: '8px',
               alignItems: 'center',
               justifyContent: 'center',
+              width: '100%',
             }}
           >
             <Typography
               sx={{
                 fontFamily: 'roboto',
                 fontSize: '14px',
-                alignSelf: 'baseline',
                 textTransform: 'none',
               }}
             >
               Sair
-            </Typography>{' '}
+            </Typography>
             <Power size={14} />
-          </Button>
-        </Container>
-      </Box>
+          </Box>
+        </MenuItem>
+      </Menu>
       {children}
     </>
   )
--- challenge-2/src/layout/private-layout.tsx
+++ challenge-2/src/layout/private-layout.tsx
@@ -1,7 +1,10 @@
-import { useAppDispatch } from '@/store/store'
+import { APP_ROUTES } from '@/context/auth-context'
+import { useAppDispatch, useAppSelector } from '@/store/store'
 import { logout } from '@/store/thunk/auth-thunk'
-import { Button } from '@mui/material'
-import { ReactNode } from 'react'
+import { Box, Button, Container, Link, Typography, IconButton, Menu, MenuItem } from '@mui/material'
+import { Power, Menu as MenuIcon } from 'lucide-react'
+import Image from 'next/image'
+import { ReactNode, useState } from 'react'
 
 interface PrivateLayoutProps {
   children: ReactNode
@@ -9,15 +12,181 @@ interface PrivateLayoutProps {
 
 export function PrivateLayout({ children }: PrivateLayoutProps) {
   const dispatch = useAppDispatch()
+  const { isAdmin } = useAppSelector((state) => state.auth)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
 
   function logoutApp() {
     dispatch(logout())
   }
 
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <>
-      PrivateLayout
-      <Button onClick={logoutApp}>Logout</Button>
+      <Box component={'header'} sx={{ background: '#692746', color: 'white', padding: '16px' }}>
+        <Container
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'row-reverse', md: 'row' },
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            fontWeight: '500',
+          }}
+        >
+          {/* Hamburger Menu Icon - visível apenas em telas menores */}
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Link href={'/'}>
+              <Image src={'/dynamox-logo-white.svg'} alt="Logo Dynamox" width={96} height={40} />
+            </Link>
+          </Box>
+
+          <Typography
+            sx={{ fontSize: '16px', fontWeight: 600, display: { xs: 'none', md: 'block' } }}
+          >
+            Eventos da dynamox
+          </Typography>
+
+          {/*DESKTOP */}
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: '8px' }}>
+            {isAdmin && (
+              <Link
+                href={APP_ROUTES.admin.dashboard}
+                underline="none"
+                sx={{
+                  background: '#ECB340',
+                  color: '#333333',
+                  display: 'flex',
+                  gap: '8px',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  padding: '6px 16px',
+                  borderRadius: '4px',
+                  fontWeight: 500,
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontFamily: 'roboto',
+                    fontSize: '14px',
+                    alignSelf: 'baseline',
+                    textTransform: 'none',
+                  }}
+                >
+                  Dashboard Admin
+                </Typography>
+              </Link>
+            )}
+
+            <Button
+              onClick={logoutApp}
+              variant="outlined"
+              sx={{
+                color: 'white',
+                display: 'flex',
+                gap: '8px',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: '1px solid white',
+              }}
+            >
+              <Typography
+                sx={{
+                  fontFamily: 'roboto',
+                  fontSize: '14px',
+                  alignSelf: 'baseline',
+                  textTransform: 'none',
+                }}
+              >
+                Sair
+              </Typography>
+              <Power size={14} />
+            </Button>
+          </Box>
+
+          {/* MOBILE */}
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleMenuClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            <MenuItem onClick={handleMenuClose}>
+              <Typography sx={{ fontSize: '16px', fontWeight: 600 }}>Eventos da dynamox</Typography>
+            </MenuItem>
+
+            {isAdmin && (
+              <MenuItem onClick={handleMenuClose}>
+                <Link
+                  href={APP_ROUTES.admin.dashboard}
+                  underline="none"
+                  sx={{
+                    background: '#ECB340',
+                    color: '#333333',
+                    display: 'flex',
+                    gap: '8px',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '6px 16px',
+                    borderRadius: '4px',
+                    fontWeight: 500,
+                    width: '100%',
+                  }}
+                >
+                  Dashboard Admin
+                </Link>
+              </MenuItem>
+            )}
+
+            <MenuItem onClick={logoutApp}>
+              <Box
+                sx={{
+                  color: '#692746',
+                  display: 'flex',
+                  gap: '8px',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '100%',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontFamily: 'roboto',
+                    fontSize: '14px',
+                    textTransform: 'none',
+                  }}
+                >
+                  Sair
+                </Typography>
+                <Power size={14} />
+              </Box>
+            </MenuItem>
+          </Menu>
+        </Container>
+      </Box>
       {children}
     </>
   )
