1e88a659b32ee6ccfcbf5c0760c2b7197c2a5d80
albanogabriel <albanogabriel33@gmail.com>
2025-04-28 20:41:36 -0300

    :sparkles: feat: use filteredEvent instead event from store

--- challenge-2/src/app/(private)/(home)/page.tsx
+++ challenge-2/src/app/(private)/(home)/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 
-import { Container, Typography } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 import { EventCardContainer } from './components/event-card-container'
+import { EventFilters } from './components/event-filters'
 
 export default function Home() {
   return (
     <div>
-      <Container sx={{ marginTop: { xs: '40px', lg: '80px' } }}>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '8px',
+          marginTop: { xs: '40px', lg: '80px' },
+        }}
+      >
         <Typography variant="h3">Eventos Dynamox</Typography>
+
+        <Box sx={{ display: 'flex', width: '100%', justifyContent: 'end', alignItems: 'center' }}>
+          <EventFilters />
+        </Box>
+
         <EventCardContainer />
       </Container>
     </div>
