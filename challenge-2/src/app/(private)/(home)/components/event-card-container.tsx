f83e08edca7da5a1b8f38ffc7c379ba15dd2bed4
albanogabriel <albanogabriel33@gmail.com>
2025-04-29 11:59:09 -0300

    :lipstick: style: improve event card grid

--- challenge-2/src/app/(private)/(home)/components/event-card-container.tsx
+++ challenge-2/src/app/(private)/(home)/components/event-card-container.tsx
@@ -1,15 +1,10 @@
 import { EventCard } from '@/_components/event-card'
+import { urlImages } from '@/constants/event-images'
 import { useAppDispatch, useAppSelector } from '@/store/store'
 import { getEvents } from '@/store/thunk/event-thunk'
 import { getRandomImageUrl } from '@/utils/get-random-image'
-import { Box, Skeleton } from '@mui/material'
+import { Box, Skeleton, Typography } from '@mui/material'
 import { useEffect } from 'react'
-
-const urlImages = [
-  'https://i0.wp.com/storage.googleapis.com/dyx-communications-mkt-public-assets/Blog/Imagens/maquina_de_papel02.jpeg?w=750&ssl=1',
-  'https://i0.wp.com/content.dynamox.net/wp-content/uploads/2024/11/Case-de-Sucesso-Cal-Trevo-imagens-1-scaled.jpg?w=2250&ssl=1',
-  'https://dynamox.net/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fccorp-public-assets%2FInstitutional%2FSuccessHistory%2FFS%2Fgeneral.webp&w=3840&q=75',
-]
 
 export function EventCardContainer() {
   const { filteredEvents, isLoading } = useAppSelector((state) => state.events)
@@ -27,14 +22,17 @@ export function EventCardContainer() {
     )
   }
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
+      <Typography sx={{ textAlign: 'end', fontSize: '12px', fontWeight: 600 }}>
+        Encontrado ({filteredEvents.length}) Eventos
+      </Typography>
+
       <Box
         sx={{
           display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(340px, 1fr))',
-          gap: { xs: '24px', lg: '16px' },
-          mt: '40px',
-          mb: '40px',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(340px, 1fr))',
+          gap: { xs: '24px', lg: '24px' },
+          justifyContent: filteredEvents.length === 1 ? 'center' : 'start',
         }}
       >
         {isLoading ? (
@@ -56,6 +54,6 @@ export function EventCardContainer() {
           </>
         )}
       </Box>
-    </>
+    </Box>
   )
 }
