44c299cfecdc6d43986cbdadfb116a3d6c246209
albanogabriel <albanogabriel33@gmail.com>
2025-04-29 11:57:55 -0300

    :sparkles: feat: create link and hover to event-card

--- challenge-2/src/_components/event-card.tsx
+++ challenge-2/src/_components/event-card.tsx
@@ -8,6 +8,7 @@ import { PencilLineIcon, Trash2 } from 'lucide-react'
 import { useState } from 'react'
 import { EventModal } from './event-modal'
 import { isEventPast } from '@/utils/check-event-date'
+import Link from 'next/link'
 
 interface EventCardProps {
   data: Event
@@ -52,36 +53,52 @@ export function EventCard({ data, randomImg }: EventCardProps) {
         />
       )}
 
-      <Card
-        sx={{ maxWidth: '100%', minWidth: '340px', border: 'none', opacity: isPastEvent ? 0.6 : 1 }}
-      >
-        <CardMedia sx={{ height: 160 }} image={randomImg} title="green iguana" />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {data.event_name}
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {data.description}
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <Link href={`events/${data.id}`} onClick={() => dispatch(setSelectedEvent(data))}>
+        <Card
+          sx={{
+            cursor: 'pointer',
+            maxWidth: '100%',
+            minWidth: '340px',
+            border: 'none',
+            opacity: isPastEvent ? 0.6 : 1,
+            transition: 'transform 0.2s ease-in-out',
+            '&:hover': {
+              transform: 'scale(1.02)',
+            },
+          }}
+        >
+          <CardMedia sx={{ height: 160 }} image={randomImg} title="green iguana" />
           <CardContent>
-            <Button size="small">{formatDate(data.date_time)}</Button>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.event_name}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {data.description}
+            </Typography>
           </CardContent>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+            <CardContent>
+              <Button size="small">{formatDate(data.date_time)}</Button>
+            </CardContent>
 
-          {isAdmin && (
-            <CardActions>
-              <Button size="small" sx={{ minWidth: '0px' }} onClick={() => handleEditEvent(data)}>
-                <PencilLineIcon size={16} />
-              </Button>
+            {isAdmin && (
+              <CardActions>
+                <Button size="small" sx={{ minWidth: '0px' }} onClick={() => handleEditEvent(data)}>
+                  <PencilLineIcon size={16} />
+                </Button>
 
-              <Button size="small" sx={{ minWidth: '0px' }} onClick={() => handleDeleteEvent(data)}>
-                <Trash2 size={15} />
-              </Button>
-            </CardActions>
-          )}
-        </Box>
-      </Card>
+                <Button
+                  size="small"
+                  sx={{ minWidth: '0px' }}
+                  onClick={() => handleDeleteEvent(data)}
+                >
+                  <Trash2 size={15} />
+                </Button>
+              </CardActions>
+            )}
+          </Box>
+        </Card>
+      </Link>
     </>
   )
 }
