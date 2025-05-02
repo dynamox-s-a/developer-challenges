26cacbc86b9ff7970d11b097b8858c9d1cfa6409
Renato Marinho <renato.marinhofr@gmail.com>
2025-05-02 14:29:25 -0300

    Update code formatting and convert quotes from single to double

--- challenge-2/cypress.config.ts
+++ challenge-2/cypress.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.ts',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    setupNodeEvents() {
-      // implement node event listeners here if needed
-    },
-  },
-})
+	e2e: {
+		baseUrl: "http://localhost:3000",
+		supportFile: "cypress/support/e2e.ts",
+		specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+		setupNodeEvents() {},
+	},
+});
--- challenge-2/cypress/support/e2e.ts
+++ challenge-2/cypress/support/e2e.ts
@@ -1,10 +1,8 @@
-import './commands'
-import '@testing-library/cypress/add-commands'
+import "./commands";
+import "@testing-library/cypress/add-commands";
 
 declare global {
-  namespace Cypress {
-    interface Chainable {
-      // Add custom commands here if needed
-    }
-  }
+	namespace Cypress {
+		interface Chainable {}
+	}
 }
