e4274f2f42fb2896745108d6935869ea8b9f4ae2
Giovanni C <ggcurcuruto@gmail.com>
2025-05-28 00:31:05 -0300

    Fix repo

--- backend/dyna-backend/src/machine/dto/updates-partial.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateMachinesDto, CreatePointOfMonitoringDTO } from "./machine";
-
-
-export class UpdateMachinesDTO extends PartialType(CreateMachinesDto){}
