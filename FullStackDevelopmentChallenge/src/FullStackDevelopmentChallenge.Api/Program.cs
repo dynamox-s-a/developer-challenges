0c5451fe99a6fa41346ec550787a967de8e1eb75
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 20:02:12 -0300

    feat: implement the Cors Policy for front end

--- FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Api/Program.cs
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Api/Program.cs
@@ -4,12 +4,25 @@ using FullStackDevelopmentChallenge.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173", "http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
@@ -23,12 +36,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowFrontend");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-
-app.UseMiddleware<ExceptionMiddleware>();
+ app.UseMiddleware<ExceptionMiddleware>();
 
 
 app.MapControllers();
