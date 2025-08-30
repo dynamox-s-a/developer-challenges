ac59f66c7d11566887cabed889fd7328d6919906
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:50:55 -0300

    feat: add get all machines use case

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/GetAll/GetAllMachinesUseCase.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using FullStackDevelopmentChallenge.Communication.Responses;
+using FullStackDevelopmentChallenge.Domain.Repositories.Specific;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.GetAll;
+public class GetAllMachinesUseCase : IGetAllMachinesUseCase
+{
+    private readonly IMachineRepository _machineRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllMachinesUseCase(IMachineRepository machineRepository, IMapper mapper)
+    {
+        _machineRepository = machineRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<GetAllMachinesResponse> GetAllMachines()
+    {
+        var result = await _machineRepository.GetAllAsync(m => m.MachineType);
+
+        return new GetAllMachinesResponse
+        {
+            Machines = _mapper.Map<List<GetMachineResponse>>(result)
+        };
+    }
+}
---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/GetAll/IGetAllMachinesUseCase.cs
@@ -0,0 +1,12 @@
+﻿using FullStackDevelopmentChallenge.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.GetAll;
+public interface IGetAllMachinesUseCase
+{
+    Task<GetAllMachinesResponse> GetAllMachines();
+}
