e20bd9b9e1deb2b865333a6dfb39254747c49b07
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:55:48 -0300

    feat: add communication classes for GET endpoints

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Communication/Responses/GetAllMachinesResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace FullStackDevelopmentChallenge.Communication.Responses;
+public class GetAllMachinesResponse
+{
+    public List<GetMachineResponse> Machines { get; set; } = new List<GetMachineResponse>();
+}
---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Communication/Responses/GetMachineResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FullStackDevelopmentChallenge.Communication.Responses;
+public class GetMachineResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string SerialNumber { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string MachineType { get; set; } = null!;
+}
