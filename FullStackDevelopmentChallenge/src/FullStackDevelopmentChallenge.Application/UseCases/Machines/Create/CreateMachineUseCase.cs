74fcc2422deeb26017fb289a4c2e8903e84c4bd0
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:50:29 -0300

    feat: add validation for Machine properties

--- FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/Create/CreateMachineUseCase.cs
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/Create/CreateMachineUseCase.cs
@@ -4,6 +4,7 @@ using FullStackDevelopmentChallenge.Communication.Responses;
 using FullStackDevelopmentChallenge.Domain.Entities;
 using FullStackDevelopmentChallenge.Domain.Repositories;
 using FullStackDevelopmentChallenge.Domain.Repositories.Specific;
+using FullStackDevelopmentChallenge.Exceptions.ExeceptionsBase;
 
 namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.Created;
 public class CreateMachineUseCase : ICreateMachineUseCase
@@ -21,6 +22,8 @@ public class CreateMachineUseCase : ICreateMachineUseCase
 
     public async Task<CreateMachineResponse> CreateMachine(CreateMachineRequest request)
     {
+        this.Validate(request);
+
         var machine = _mapper.Map<Machine>(request);
         machine.Id = Guid.NewGuid();
 
@@ -28,5 +31,19 @@ public class CreateMachineUseCase : ICreateMachineUseCase
         await _unitOfWork.Commit();
 
         return _mapper.Map<CreateMachineResponse>(machine);
+    }
+
+    private void Validate(CreateMachineRequest request)
+    {
+        var validator = new MachineValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
     }
 }
