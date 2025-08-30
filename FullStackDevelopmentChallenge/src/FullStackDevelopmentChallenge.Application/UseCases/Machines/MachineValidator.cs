ab8f9d024e26fce1952b4122a80da7b1e1994aba
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:51:43 -0300

    feat: add machine validator

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/MachineValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FullStackDevelopmentChallenge.Exceptions;
+
+using FullStackDevelopmentChallenge.Communication.Requests;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines;
+public class MachineValidator : AbstractValidator<CreateMachineRequest>
+{
+    public MachineValidator()
+    {
+        RuleFor(expense => expense.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_REQUIRED);
+        RuleFor(expense => expense.SerialNumber).NotEmpty().WithMessage(ResourceErrorMessages.SERIAL_NUMBER_REQUIRED);
+        RuleFor(expense => expense.MachineTypeId).NotEmpty().WithMessage(ResourceErrorMessages.MACHINE_TYPE_REQUIRED);
+    }
+}
