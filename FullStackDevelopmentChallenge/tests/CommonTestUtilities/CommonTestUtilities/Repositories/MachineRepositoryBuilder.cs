4bbc7c3b4fbb9ed86793173c3d405f80741354f4
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 13:20:16 -0300

    test: implement the mock of the unit of work and Machine Repository

---
+++ FullStackDevelopmentChallenge/tests/CommonTestUtilities/CommonTestUtilities/Repositories/MachineRepositoryBuilder.cs
@@ -0,0 +1,43 @@
+﻿using FullStackDevelopmentChallenge.Domain.Entities;
+using FullStackDevelopmentChallenge.Domain.Repositories.Specific;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class MachineRepositoryBuilder
+{
+    private readonly Mock<IMachineRepository> _repository;
+
+    public MachineRepositoryBuilder()
+    {
+        _repository = new Mock<IMachineRepository>();
+    }
+
+    // Configura o retorno do GetAllAsync
+    public MachineRepositoryBuilder GetAll(List<Machine> machines)
+    {
+        _repository.Setup(r => r.GetAllAsync()).ReturnsAsync(machines);
+        return this;
+    }
+
+    // Configura o retorno do GetByIdAsync
+    public MachineRepositoryBuilder GetById(Machine? machine)
+    {
+        if (machine != null)
+        {
+            _repository.Setup(r => r.GetByIdAsync(machine.Id))
+                       .ReturnsAsync(machine);
+        }
+        return this;
+    }
+
+    // Para permitir que o GetByIdAsync com qualquer GUID seja configurado
+    public MachineRepositoryBuilder GetByIdAny(List<Machine> machines)
+    {
+        _repository.Setup(r => r.GetByIdAsync(It.IsAny<Guid>()))
+                   .ReturnsAsync((Guid id) => machines.FirstOrDefault(m => m.Id == id));
+        return this;
+    }
+
+    // Retorna o objeto mockado
+    public IMachineRepository Build() => _repository.Object;
+}
