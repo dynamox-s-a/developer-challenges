7285fc6f582e913b4a36a4ff48bdecb3c7aa3cb6
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-29 21:40:10 -0300

    feat: implement machine creation use case

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/Create/CreateMachineUseCase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using FullStackDevelopmentChallenge.Communication.Requests;
+using FullStackDevelopmentChallenge.Communication.Responses;
+using FullStackDevelopmentChallenge.Domain.Entities;
+using FullStackDevelopmentChallenge.Domain.Repositories;
+using FullStackDevelopmentChallenge.Domain.Repositories.Specific;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.Created;
+public class CreateMachineUseCase : ICreateMachineUseCase
+{
+    private readonly IMachineRepository _machineRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateMachineUseCase(IMachineRepository machineRepository, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _machineRepository = machineRepository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<CreateMachineResponse> CreateMachine(CreateMachineRequest request)
+    {
+        var machine = _mapper.Map<Machine>(request);
+        machine.Id = Guid.NewGuid();
+
+        await _machineRepository.CreateAsync(machine);
+        await _unitOfWork.Commit();
+
+        return _mapper.Map<CreateMachineResponse>(machine);
+    }
+}
---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/Create/ICreateMachineUseCase.cs
@@ -0,0 +1,13 @@
+﻿using FullStackDevelopmentChallenge.Communication.Requests;
+using FullStackDevelopmentChallenge.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.Created;
+public interface ICreateMachineUseCase
+{
+    Task<CreateMachineResponse> CreateMachine(CreateMachineRequest request);
+}
