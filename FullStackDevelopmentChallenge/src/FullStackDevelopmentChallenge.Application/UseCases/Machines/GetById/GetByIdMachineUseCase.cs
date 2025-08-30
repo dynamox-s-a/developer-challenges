00747888cabaa4ed97870b8219ff99d5b049b4f1
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:51:25 -0300

    feat: add get by id machine use case

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/GetById/GetByIdMachineUseCase.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FullStackDevelopmentChallenge.Communication.Responses;
+using FullStackDevelopmentChallenge.Domain.Repositories.Specific;
+using FullStackDevelopmentChallenge.Exceptions;
+using FullStackDevelopmentChallenge.Exceptions.ExeceptionsBase;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.GetById;
+public class GetByIdMachineUseCase : IGetByIdMachineUseCase
+{
+    private readonly IMachineRepository _machineRepository;
+    private readonly IMapper _mapper;
+
+    public GetByIdMachineUseCase(IMachineRepository machineRepository, IMapper mapper)
+    {
+        _machineRepository = machineRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<GetMachineResponse> GetById(Guid id)
+    {
+        var machine = await _machineRepository.GetByIdAsync(id, m => m.MachineType);
+
+        if (machine is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.MACHINE_NOT_FOUND);
+        }
+
+        return _mapper.Map<GetMachineResponse>(machine);
+    }
+}
---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Application/UseCases/Machines/GetById/IGetByIdMachineUseCase.cs
@@ -0,0 +1,12 @@
+﻿using FullStackDevelopmentChallenge.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStackDevelopmentChallenge.Application.UseCases.Machines.GetById;
+public interface IGetByIdMachineUseCase
+{
+    Task<GetMachineResponse> GetById(Guid id);
+}
