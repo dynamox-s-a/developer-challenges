7251cdfd979db37371da4b92ec14f952e0ba68d4
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:56:08 -0300

    feat: add communication class error for endpoints

---
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Communication/Responses/ResponseError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStackDevelopmentChallenge.Communication.Responses;
+public class ResponseError
+{
+    public List<string> ErrorMessages { get; set; }
+
+    public ResponseError(string errorMessage)
+    {
+        ErrorMessages = [errorMessage];
+    }
+
+    public ResponseError(List<string> errorMessage)
+    {
+        ErrorMessages = errorMessage;
+    }
+}
