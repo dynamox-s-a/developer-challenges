c62d373fd8ae2c375d4336262d4c2f2a9853da3a
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:49:17 -0300

    feat: add get all and get by id endpoints

--- FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Api/Controllers/MachineController.cs
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Api/Controllers/MachineController.cs
@@ -1,4 +1,6 @@
 ﻿using FullStackDevelopmentChallenge.Application.UseCases.Machines.Created;
+using FullStackDevelopmentChallenge.Application.UseCases.Machines.GetAll;
+using FullStackDevelopmentChallenge.Application.UseCases.Machines.GetById;
 using FullStackDevelopmentChallenge.Communication.Requests;
 using FullStackDevelopmentChallenge.Communication.Responses;
 using Microsoft.AspNetCore.Http;
@@ -11,13 +13,39 @@ public class MachineController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(CreateMachineResponse), StatusCodes.Status201Created)]
-    //[ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Register(
+    [ProducesResponseType(typeof(ResponseError), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create(
     [FromServices] ICreateMachineUseCase useCase,
     [FromBody] CreateMachineRequest request)
     {
         var response = await useCase.CreateMachine(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(GetAllMachinesResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAll([FromServices] IGetAllMachinesUseCase useCase)
+    {
+        var response = await useCase.GetAllMachines();
+
+        if (response.Machines.Count != 0)
+            return Ok(response);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(GetMachineResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseError), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById(
+    [FromServices] IGetByIdMachineUseCase useCase,
+    [FromRoute] Guid id)
+    {
+        var response = await useCase.GetById(id);
+
+        return Ok(response);
     }
 }
