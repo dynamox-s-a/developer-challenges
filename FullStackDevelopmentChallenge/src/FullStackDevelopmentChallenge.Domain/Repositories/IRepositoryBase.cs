18cbf695cfb26fd1c090f3e7127ff1039071629d
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-30 10:58:34 -0300

    feat: add support for include parameters in queries

--- FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Domain/Repositories/IRepositoryBase.cs
+++ FullStackDevelopmentChallenge/src/FullStackDevelopmentChallenge.Domain/Repositories/IRepositoryBase.cs
@@ -1,7 +1,9 @@
-﻿namespace FullStackDevelopmentChallenge.Domain.Repositories;
+﻿using System.Linq.Expressions;
+
+namespace FullStackDevelopmentChallenge.Domain.Repositories;
 public interface IRepositoryBase<T> where T : class
 {
-    Task<T> GetByIdAsync(Guid id);
-    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes);
+    Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
     Task CreateAsync(T entity);
 }
