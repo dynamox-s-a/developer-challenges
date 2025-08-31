1968313daeae7bab651eec2714e18ea18e7edfb2
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-31 15:58:45 -0300

    feat: add the machineType property in Machine model

--- FullStackDevelopmentChallengeFront/src/types/Machine.ts
+++ FullStackDevelopmentChallengeFront/src/types/Machine.ts
@@ -1,8 +1,10 @@
 export interface Machine {
+  id: string;
   name: string;
   description: string;
   serialNumber: string;
   machineTypeId: string;
+  machineType: string;
 }
 
 export const machineTypes = ["B53F0ABF-7D3B-40B8-925A-144DC694397C", "91693BD7-6D1F-40DE-9ED5-E779D201ED90", "D64B2669-C476-4965-A5DA-1CE9FF99B0CF"];
