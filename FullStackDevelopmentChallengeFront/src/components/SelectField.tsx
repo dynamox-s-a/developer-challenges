ecb8cf119a29402406b6aa1bd36d84d3366587e2
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-31 19:52:28 -0300

    feat: implement the generic select field

--- FullStackDevelopmentChallengeFront/src/components/SelectField.tsx
+++ FullStackDevelopmentChallengeFront/src/components/SelectField.tsx
@@ -1,29 +1,25 @@
 import React, { forwardRef } from "react";
 
 type SelectFieldProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
-  label: string;
   options: string[];
 };
 
 const SelectField = forwardRef<HTMLSelectElement, SelectFieldProps>(
-  ({ label, options, ...props }, ref) => {
+  ({ options, ...props }, ref) => {
     return (
-      <div>
-        <label className="block mb-2 text-gray-700 font-medium">{label}</label>
-        <select
-          ref={ref}
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg text-gray-900 
-                     focus:outline-none focus:ring-2 focus:ring-blue-400"
-          {...props}
-        >
-          <option value="">Selecione um tipo</option>
-          {options.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
-        </select>
-      </div>
+      <select
+        ref={ref}
+        className="w-full px-4 py-2 border border-gray-300 rounded-lg text-gray-900 
+                   focus:outline-none focus:ring-2 focus:ring-blue-400"
+        {...props}
+      >
+        <option value="">Selecione um tipo</option>
+        {options.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
+        ))}
+      </select>
     );
   }
 );
