476dd6e9223367c78a34cfff8845233ae4ec27e1
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-31 20:49:37 -0300

    refactor: implement the machine type service to populate the dropdown in create page

--- FullStackDevelopmentChallengeFront/src/services/machineService.ts
+++ FullStackDevelopmentChallengeFront/src/services/machineService.ts
@@ -5,14 +5,14 @@ import { type Machine } from "../types/Machine";
 import type { MachineResponse } from "../types/MachineResponse";
 
 export const createMachine = (machine: CreateMachineRequest) => {
-  return api.post("/", machine);
+  return api.post("/Machine/", machine);
 };
 export const getByIdMachine = async(id: string) : Promise<MachineResponse>  => {
-  const response = await api.get<Machine>(`/${id}`);
+  const response = await api.get<Machine>(`/Machine/${id}`);
   return response.data;
 };
 
 export const getListMachine = async (): Promise<MachineResponse[]> => {
-  const response = await api.get<{ machines: MachineResponse[] }>("/");
+  const response = await api.get<{ machines: MachineResponse[] }>("/Machine");
   return response.data.machines;
 };
