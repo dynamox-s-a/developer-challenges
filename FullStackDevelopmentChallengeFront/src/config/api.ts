43983b179bcc7f17f16ece2d4f5c02b175111854
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-31 20:47:34 -0300

    feat: implement enviroment variable to backend url

--- FullStackDevelopmentChallengeFront/src/config/api.ts
+++ FullStackDevelopmentChallengeFront/src/config/api.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
-export const api =axios.create({
-    baseURL: 'https://localhost:7024/Machine'
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
