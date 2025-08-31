588cba6615a656613fbf1ef94634686b01bc35ea
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-31 15:57:54 -0300

    fix: corrected the endpoints

--- FullStackDevelopmentChallengeFront/src/services/machineServices.ts
+++ FullStackDevelopmentChallengeFront/src/services/machineServices.ts
@@ -1,13 +1,18 @@
 // services/machineService.ts
 import { api } from "../config/api";
+import type { CreateMachineRequest } from "../types/CreateMachineRequest";
 import { type Machine } from "../types/Machine";
+import type { MachineResponse } from "../types/MachineResponse";
 
-export const createMachine = (machine: Machine) => {
+export const createMachine = (machine: CreateMachineRequest) => {
   return api.post("/", machine);
 };
-export const getByIdMachine = (machine: Machine) => {
-  return api.post(`/${machine.machineTypeId}`);
+export const getByIdMachine = async(id: string) : Promise<MachineResponse>  => {
+  const response = await api.get<Machine>(`/${id}`);
+  return response.data;
 };
-export const getListMachine = () => {
-  return api.get("/");
+
+export const getListMachine = async (): Promise<MachineResponse[]> => {
+  const response = await api.get<{ machines: MachineResponse[] }>("/");
+  return response.data.machines;
 };
