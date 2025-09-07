12514e176d3f6c5516d4a5e3149130236a531ffb
Matheus Aguiar <mathscndev@gmail.com>
2025-09-07 15:12:50 -0300

    chore: setup initial project structure with Vite + TypeScript + MUI (styled-components engine)

---
+++ front-end-challenge-v2/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    },
+  },
+})
