f704e06be91f5f04272e37c2cc367d9f5f47091c
jaoleonardo01 <joaoleonardo09@gmail.com>
2025-11-22 17:15:19 -0300

    test: add charts validation (hovering/tooltip)

--- qa-challenge/cypress/e2e/web/web-charts.cy.js
+++ qa-challenge/cypress/e2e/web/web-charts.cy.js
@@ -13,6 +13,112 @@ describe('Web GUI Test - Charts', () => {
     cy.get('.highcharts-graph').eq(0).should('be.visible');
     cy.get('.highcharts-graph').eq(1).should('be.visible');
     cy.get('.highcharts-graph').eq(2).should('be.visible');
-    cy.get('.highcharts-graph').eq(3).should('be.visible');
+
+    // 3. Charts hovering:
+    
+    // Aceleração RMS chart - mouse bouncing to trigger the tooltip  
+    cy.get('rect.highcharts-background').eq(0).then(($rect) => {
+      const width = parseFloat($rect.attr('width'));
+      const height = parseFloat($rect.attr('height'));
+
+      const centerX = width / 2;
+      const centerY = height / 2;
+
+      const $el = cy.wrap($rect);
+
+      $el.realMouseMove(centerX, centerY);
+
+      const moves = [
+        { x: centerX + 5, y: centerY },
+        { x: centerX + 5, y: centerY + 5 },
+        { x: centerX, y: centerY + 5 },
+        { x: centerX - 5, y: centerY },
+        { x: centerX, y: centerY - 5 },
+        { x: centerX, y: centerY }
+      ];
+
+      moves.forEach((pos) => {
+        $el.realMouseMove(pos.x, pos.y);
+      });
+    });
+
+    // Aceleração RMS chart -checking tooltip
+    cy.get('.highcharts-container').eq(0).find('path.highcharts-label-box.highcharts-tooltip-box')
+    .then(($el) => {
+      if ($el.length === 0) {
+        cy.log('Hovering/tooltip issues for Aceleração RMS chart');
+        throw new Error('Hovering/tooltip issues for Aceleração RMS chart');
+      }
+    });
+
+    // Temperatura - mouse bouncing to trigger the tooltip  
+    cy.get('rect.highcharts-background').eq(1).then(($rect) => {
+      const width = parseFloat($rect.attr('width'));
+      const height = parseFloat($rect.attr('height'));
+
+      const centerX = width / 2;
+      const centerY = height / 2;
+
+      const $el = cy.wrap($rect);
+
+      $el.realMouseMove(centerX, centerY);
+
+      const moves = [
+        { x: centerX + 5, y: centerY },
+        { x: centerX + 5, y: centerY + 5 },
+        { x: centerX, y: centerY + 5 },
+        { x: centerX - 5, y: centerY },
+        { x: centerX, y: centerY - 5 },
+        { x: centerX, y: centerY }
+      ];
+
+      moves.forEach((pos) => {
+        $el.realMouseMove(pos.x, pos.y);
+      });
+    });
+
+    // Temperatura -checking tooltip
+    cy.get('.highcharts-container').eq(1).find('path.highcharts-label-box.highcharts-tooltip-box')
+    .then(($el) => {
+      if ($el.length === 0) {
+        cy.log('Hovering/tooltip issues for Temperatura chart');
+        throw new Error('Hovering/tooltip issues for Aceleração RMS chart');
+      }
+    });
+
+    // Velocidade RMS - mouse bouncing to trigger the tooltip  
+    cy.get('rect.highcharts-background').eq(2).then(($rect) => {
+      const width = parseFloat($rect.attr('width'));
+      const height = parseFloat($rect.attr('height'));
+
+      const centerX = width / 2;
+      const centerY = height / 2;
+
+      const $el = cy.wrap($rect);
+
+      $el.realMouseMove(centerX, centerY);
+
+      const moves = [
+        { x: centerX + 5, y: centerY },
+        { x: centerX + 5, y: centerY + 5 },
+        { x: centerX, y: centerY + 5 },
+        { x: centerX - 5, y: centerY },
+        { x: centerX, y: centerY - 5 },
+        { x: centerX, y: centerY }
+      ];
+
+      moves.forEach((pos) => {
+        $el.realMouseMove(pos.x, pos.y);
+      });
+    });
+
+    // Velocidade RMS -checking tooltip
+    cy.get('.highcharts-container').eq(2).find('path.highcharts-label-box.highcharts-tooltip-box')
+    .then(($el) => {
+      if ($el.length === 0) {
+        cy.log('Hovering/tooltip issues for Velocidade RMS chart');
+        throw new Error('Hovering/tooltip issues for Aceleração RMS chart');
+      }
+    });
   })
 })
