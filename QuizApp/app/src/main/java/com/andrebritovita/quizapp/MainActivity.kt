4178939d07b25ba78b55cc7e8b451698e221fcce
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-11-28 10:12:45 -0300

    config(core): add internet permission and Hilt entry point

--- QuizApp/app/src/main/java/com/andrebritovita/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/andrebritovita/quizapp/MainActivity.kt
@@ -10,9 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.andrebritovita.quizapp.ui.theme.QuizAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +37,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizAppTheme {
-        Greeting("Android")
-    }
 }
