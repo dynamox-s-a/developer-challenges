8c2f15e8dbffff5e1954f47dd30af12a1976d94e
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-11-28 17:11:09 -0300

    feat(domain): implement GetNewQuestionUseCase with duplicate filtering logic

---
+++ QuizApp/app/src/main/java/com/andrebritovita/quizapp/domain/usecase/GetNewQuestionUseCase.kt
@@ -0,0 +1,43 @@
+package com.andrebritovita.quizapp.domain.usecase
+
+import com.andrebritovita.quizapp.domain.model.Question
+import com.andrebritovita.quizapp.domain.repository.QuizRepository
+import javax.inject.Inject
+
+
+/**
+ * Caso de Uso responsável por obter uma nova pergunta garantindo que ela não seja repetida.
+ *
+ * Utiliza uma estratégia de "tentativas" limitadas:
+ * Tenta buscar uma pergunta no repositório até [MAX_ATTEMPTS] vezes.
+ * Se a pergunta já tiver sido visualizada na sessão atual (seenIds), ela é descartada e uma nova tentativa é feita.
+ */
+class GetNewQuestionUseCase @Inject constructor (
+    private val repository: QuizRepository
+){
+    private val seenIds = mutableSetOf<String>() // Localiza mais rápido com ".contains()" (Complexidade O(1)) e é anti-duplicidade
+    private val MAX_ATTEMPTS = 5
+
+    suspend operator fun invoke(): Result<Question> {
+        repeat(MAX_ATTEMPTS) {
+            val res = repository.getQuestion()
+            if (res.isSuccess){
+                val question = res.getOrNull()!!
+                if (!seenIds.contains(question.id)){
+                    seenIds.add(question.id)
+                    return Result.success(question)
+                }
+            } else  {
+                return res
+            }
+        }
+        return Result.failure(Exception("Não foi possível encontrar uma pergunta nova após $MAX_ATTEMPTS tentativas."))
+    }
+    /**
+     * Limpa a memória de perguntas vistas.
+     * Usado quando o jogador clica em "Jogar Novamente".
+     */
+    fun reset(){
+        seenIds.clear()
+    }
+}
