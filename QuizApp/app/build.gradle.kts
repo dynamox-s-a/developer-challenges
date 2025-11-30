6fde335b4ee17be5c8f6d90d5e2c71b2f630f2b0
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-11-30 10:35:20 -0300

    chore(build): add kotlin-test dependency for unit testing assertions

--- QuizApp/app/build.gradle.kts
+++ QuizApp/app/build.gradle.kts
@@ -97,4 +97,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(kotlin("test"))
 }
