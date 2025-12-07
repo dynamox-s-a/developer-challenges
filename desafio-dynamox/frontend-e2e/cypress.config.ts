76a3589032d8ce9ac3f59430a7aa52a51ea8e629
Wesley Marques <wesleytmarques@outlook.com>
2025-12-07 15:04:44 -0300

    feat: setup inicial do projeto

---
+++ desafio-dynamox/frontend-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'npx nx run frontend:dev',
+        production: 'npx nx run frontend:preview',
+      },
+      ciWebServerCommand: 'npx nx run frontend:preview',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
