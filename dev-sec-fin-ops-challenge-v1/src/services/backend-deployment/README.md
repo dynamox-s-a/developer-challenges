9dd3c9ef4f652f40577d9897d5c4701af8794ea1
Matheus Roque <matheusdcroque@gmail.com>
2026-01-19 10:06:32 -0300

    Update backend service

--- dev-sec-fin-ops-challenge-v1/src/services/backend-deployment/README.md
+++ dev-sec-fin-ops-challenge-v1/src/services/backend-deployment/README.md
@@ -1,12 +1,26 @@
 # Backend Deployment Service
 
+Small service that listens to `localhost:8000` and counts the amount of calls received in `/` and `/request`, and returns the value in `/count`.
+
 ## O.S. Environment Setup
 
-* [Nodejs setup](https://nodejs.org/en/download)
-* [Python setup](https://www.python.org/downloads/)
+To run the service locally you need the following:
 
-TODO.
+- Python (>=3.14);
+- Python libraries:
+  - fastapi (>=0.128.0,<0.129.0);
+  - uvicorn (>=0.40.0,<0.41.0);
 
 ## Docker Environment Setup
 
-TODO.
+Build the Docker image with:
+
+```bash
+docker-compose build
+```
+
+After building the image, run the it with the following command:
+
+```bash
+docker run -p 8000:8000 backend-deployment-count-api:latest
+```
--- dev-sec-fin-ops-challenge-v1/src/services/backend-deployment/api/main.py
+++ dev-sec-fin-ops-challenge-v1/src/services/backend-deployment/api/main.py
@@ -1,51 +1,58 @@
+import json
+
 from fastapi import FastAPI
 from fastapi.responses import Response
 
 PERSISTENCE_FILE = "requests.json"
 
+def read_count():
+  request_count = 0
+  with open(PERSISTENCE_FILE, "r") as f:
+    request_count = json.load(f).get("requests")
+  return request_count
+
+def read_json():
+  with open(PERSISTENCE_FILE, "r") as f:
+    data = json.load(f)
+    return data
+
+def write_count(new_value: int):
+  with open(PERSISTENCE_FILE, "w+") as f:
+    json.dump({"requests": new_value}, f)
+  
+def increment_count():
+  value = read_count()
+  new_value = value +1
+  write_count(new_value)
+  return new_value
+
 async def lifespan(app: FastAPI):
   # init
   with open(PERSISTENCE_FILE, "w+") as f:
-    f.write("0")
+    json.dump({"requests": 0}, f)
 
   # provide API
   yield
 
   # shutdown
-  with open(PERSISTENCE_FILE, "r") as f:
-    print(f"Final count: {f.read()}")
+  print(f"Final count: {read_json()}")
 
 app = FastAPI(lifespan=lifespan)
-
-def read():
-  value = 0
-  with open(PERSISTENCE_FILE, "r") as f:
-    value = int(f.read().strip())
-  return value
-
-def write(new_value: int):
-  with open(PERSISTENCE_FILE, "w+") as f:
-    f.write(str(new_value))
-  
-def increment():
-  value = read()
-  new_value = value +1
-  write(new_value)
-  return new_value
 
 @app.get("/")
 @app.get("/request")
 def increment_request_counter():
   try:
-    value = increment()
-    return {"status": Response(status_code=200)}
+    increment_count()
+    value = read_json()
+    return value
   except:
     return {"status": Response(status_code=500)}
 
 @app.get("/count")
 def request_count():
   try:
-    value = read()
+    value = read_json()
     return value
   except:
     return {"status": Response(status_code=500)}
