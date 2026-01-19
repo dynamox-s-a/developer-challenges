ec07c04b40f696a1e9a04359ac3f88e1fbb6b5a8
brenobmoreira <brenobmoreira20@gmail.com>
2026-01-18 21:09:58 -0300

    feat: add CRUD operations for machine management and update schemas

--- backend/app/modules/machines/router.py
+++ backend/app/modules/machines/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlmodel import Session
 from app.core.database import get_session
 from app.core.security import get_current_user
 from app.models import Machine, User
-from .schemas import MachineCreate, MachineRead
+from .schemas import MachineCreate, MachineRead, MachineUpdate
+from typing import List
+from sqlmodel import Session, select
 
 router = APIRouter()
 
@@ -24,3 +26,69 @@ def create_machine(
     session.commit()
     session.refresh(new_machine)
     return new_machine
+
+
+@router.get("/", response_model=List[MachineRead], status_code=status.HTTP_200_OK)
+def list_machines(
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    
+    list_machines = session.exec(select(Machine).where(Machine.user_id == current_user.id))
+    return list_machines
+
+
+@router.get("/{machine_id}", response_model=MachineRead, status_code=status.HTTP_200_OK)
+def get_machines(
+    machine_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    
+    machine = session.exec(select(Machine).where(Machine.id == machine_id, Machine.user_id == current_user.id)).first()
+
+    if machine is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Maquina nao encontrada")
+    
+    return machine
+
+
+@router.put("/{machine_id}", response_model=MachineRead, status_code=status.HTTP_200_OK)
+def update_machine(
+    machine_id: int,
+    machine_update: MachineUpdate,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    
+    machine = session.exec(select(Machine).where(Machine.id == machine_id, Machine.user_id == current_user.id)).first()
+
+    if machine is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Maquina nao encontrada")
+
+    update_data = machine_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(machine, key, value)
+
+    session.add(machine)
+    session.commit()
+    session.refresh(machine)
+
+    return machine
+
+
+@router.delete("/{machine_id}")
+def delete_machine(
+    machine_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    machine = session.exec(select(Machine).where(Machine.id == machine_id, Machine.user_id == current_user.id)).first()
+
+    if machine is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Maquina nao encontrada")
+
+    session.delete(machine)
+    session.commit()
+    
+    return {"message": f"Maquina com id {machine_id} deletada com sucesso"}
--- backend/app/modules/machines/schemas.py
+++ backend/app/modules/machines/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from app.models import MachineType
+from typing import Optional
 
 class MachineCreate(BaseModel):
     name: str
@@ -10,6 +11,13 @@ class MachineRead(BaseModel):
     name: str
     type: MachineType
     user_id: int
+
+    class Config:
+        from_attributes: True
+
+class MachineUpdate(BaseModel):
+    name: Optional[str] = None
+    type: Optional[MachineType] = None
 
     class Config:
         from_attributes: True
