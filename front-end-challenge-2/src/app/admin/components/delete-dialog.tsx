4e4e486b4ea301ff78c985e16c1d94d99bf0eab8
Marcos Paulo <marcospaulosousa48@gmail.com>
2026-02-06 17:18:50 -0300

    feat: create delet event component

---
+++ front-end-challenge-2/src/app/admin/components/delete-dialog.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button,
+} from "@mui/material";
+
+interface DeleteDialogProps {
+  open: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function DeleteDialog({
+  open,
+  onConfirm,
+  onCancel,
+}: DeleteDialogProps) {
+  return (
+    <Dialog open={open} onClose={onCancel}>
+      <DialogTitle>Confirmar Exclusão</DialogTitle>
+      <DialogContent>
+        <Typography>
+          Tem certeza que deseja excluir este evento? Esta ação não pode ser
+          desfeita.
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel}>Cancelar</Button>
+        <Button onClick={onConfirm} color="error" variant="contained">
+          Excluir
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
