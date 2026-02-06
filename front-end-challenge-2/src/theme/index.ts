8ede4ab609e4bb277f6a53364187e1a097376fda
Marcos Paulo <marcospaulosousa48@gmail.com>
2026-02-06 16:32:35 -0300

    feat: create theme

---
+++ front-end-challenge-2/src/theme/index.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material/styles";
+
+import { palette } from "./pallete";
+
+export const theme = createTheme({
+  palette,
+});
---
+++ front-end-challenge-2/src/theme/pallete.ts
@@ -0,0 +1,41 @@
+import { PaletteOptions } from "@mui/material/styles";
+
+export const palette: PaletteOptions = {
+  mode: "light",
+
+  primary: {
+    main: "#2563EB",
+    light: "#60A5FA",
+    dark: "#1E40AF",
+    contrastText: "#FFFFFF",
+  },
+
+  secondary: {
+    main: "#9333EA",
+    light: "#C084FC",
+    dark: "#6B21A8",
+    contrastText: "#FFFFFF",
+  },
+
+  success: {
+    main: "#16A34A",
+  },
+
+  error: {
+    main: "#DC2626",
+  },
+
+  warning: {
+    main: "#D97706",
+  },
+
+  background: {
+    default: "#F9FAFB",
+    paper: "#FFFFFF",
+  },
+
+  text: {
+    primary: "#111827",
+    secondary: "#6B7280",
+  },
+};
