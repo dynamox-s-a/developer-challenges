aa6e5215717c8ba4596c08ee5470574f3a6b22e2
Marcos Paulo <marcospaulosousa48@gmail.com>
2026-02-06 16:43:35 -0300

    feat: add providers to layout

--- front-end-challenge-2/src/app/layout.tsx
+++ front-end-challenge-2/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/providers/theme-provider";
+import StoreProvider from "@/providers/store-provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -14,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Sistema de Gestão de Eventos ",
-  description: "Uma aplicação para gerenciar eventos de forma eficiente.",
+  description: "Plataforma de gerenciamento de eventos",
 };
 
 export default function RootLayout({
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <StoreProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </StoreProvider>
       </body>
     </html>
   );
