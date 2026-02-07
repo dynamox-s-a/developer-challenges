f66fccb23f9b9fddd11604f3cbe59e9c3448e7de
Marcos Paulo <marcospaulosousa48@gmail.com>
2026-02-07 14:36:40 -0300

    test: vitest config

---
+++ front-end-challenge-2/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import path from "path";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
