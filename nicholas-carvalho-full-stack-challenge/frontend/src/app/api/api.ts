c2c09cc583c1d578bacadd0de858e1696bd25c65
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-02-09 00:10:19 -0300

    fix: url backend access problem

--- nicholas-carvalho-full-stack-challenge/frontend/src/app/api/api.ts
+++ nicholas-carvalho-full-stack-challenge/frontend/src/app/api/api.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL, withCredentials: true, timeout: 350 });
+export const api = axios.create({ baseURL: "http://localhost:3001", withCredentials: true, timeout: 350 });
