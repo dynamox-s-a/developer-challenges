6bd3176fb2c998bfb1e721412204340f70d951b7
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2026-02-12 13:26:33 -0300

    implementado testes cobrindo os requisitos das info do cabecalho e do dados atualizados ao acessar a pagina

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -9,7 +9,52 @@ test.describe('Dashboard', () => {
         await dashboardPage.visit()
     })
 
-    test('deve exibir os títulos dos gráficos', async () => {
-        await expect(dashboardPage.page.locator('h6', { hasText: /RMS|Temperatura/ })).toHaveCount(3);
+    test('deve exibir um cabeçalho com informações das maquinas', async ({ page }) => {
+        const headerTitle = page.locator('h6', { hasText: 'Análise de dados' });
+        const idMachine = page.locator('span.MuiTypography-caption').filter({ hasText: /^Máquina\s+\d+$/ });
+        const spot = page.locator('span.MuiTypography-caption').filter({ hasText: /^Ponto\s+\d+$/ });
+        const RPM = page.locator('span.MuiTypography-caption').filter({ hasText: /^\d+$/ });
+        const dynamicRange = page.locator('span.MuiTypography-caption').filter({ hasText: /^\d+g$/ });
+        const interval = page.locator('span.MuiTypography-caption').filter({ hasText: /^\d+\s*min$/i });
+
+        await expect(headerTitle).toBeVisible();
+        await expect(idMachine).toBeVisible();
+        await expect(spot).toBeVisible();
+        await expect(RPM).toBeVisible();
+        await expect(dynamicRange).toBeVisible();
+        await expect(interval).toBeVisible();
     })
+
+    test('deve exibir os títulos dos gráficos', async ({ page }) => {
+        await expect(page.locator('h6', { hasText: /RMS|Temperatura/ })).toHaveCount(3);
+    })
+
+    test('deve buscar os dados ao acessar a página', async ({ page }) => {
+
+        const dataPromise = page.waitForResponse(response =>
+            response.url().endsWith('/data.json') && response.status() === 200
+        );
+
+        const metadataPromise = page.waitForResponse(response =>
+            response.url().endsWith('/metadata.json') && response.status() === 200
+        );
+
+        await page.goto('/');
+
+        const [dataResponse, metadataResponse] = await Promise.all([
+            dataPromise,
+            metadataPromise
+        ]);
+
+        const metadataBody = await metadataResponse.json();
+        console.log(metadataBody);
+
+        expect(dataResponse.status()).toBe(200);
+        expect(metadataResponse.status()).toBe(200);
+        await expect(page.getByText(metadataBody.machine)).toBeVisible();
+        await expect(page.getByText(metadataBody.spot)).toBeVisible();
+        await expect(page.getByText(metadataBody.rpm)).toBeVisible();
+        await expect(page.getByText(metadataBody.dynamicRange)).toBeVisible();
+    });
+
 })
