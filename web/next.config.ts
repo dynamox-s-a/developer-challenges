68f2908e487faf0cacf6881afbf1280b10e3decd
NandoSchlemper <bkschlemper@gmail.com>
2026-02-13 03:44:43 -0300

    [feature] Config

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['*'],
   reactCompiler: true,
-  output: 'standalone'
+  output: 'standalone',
 }
 
 export default nextConfig
