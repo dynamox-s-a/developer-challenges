30ba3670c73ba71a428485949a2c8cf4efadb4d1
Patricia Rordigues <prodrigues@nipr.com>
2026-02-22 10:24:24 -0300

    refactor: optimize title highlight animation

--- landing-page/src/components/Hero/Hero.tsx
+++ landing-page/src/components/Hero/Hero.tsx
@@ -4,27 +4,35 @@ import { heroData } from './Hero.data';
 
 export function Hero() {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [animate, setAnimate] = useState(true);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setActiveIndex((prevIndex) => (prevIndex + 1) % heroData.titleHighlights.length);
-        }, 2400);
-        return () => clearInterval(interval);
+      const interval = setInterval(() => {
+        setAnimate(false);
+        setTimeout(() => {
+          setActiveIndex((prev) =>
+            (prev + 1) % heroData.titleHighlights.length
+          );
+          setAnimate(true);
+        }, 100);
+      }, 2400);
+
+      return () => clearInterval(interval);
     }, []);
 
   return (
     <section className={styles.hero}>
         <img src={heroData.brand.logoSrc} alt={heroData.brand.logoAlt} className={styles.hero__image} />
       <div className={styles.hero__content}>
-        <h1 className={styles.hero__title}>
-            {heroData.title}{' '}
-            <span
-            key={heroData.titleHighlights[activeIndex]}
-            className={styles.hero__titleHighlight}
-            >
-                {heroData.titleHighlights[activeIndex]}
-            </span>
-        </h1>
+              <h1 className={styles.hero__title}>
+        {heroData.title}{' '}
+        <span
+          className={`${styles.hero__titleHighlight} ${
+            animate ? styles.animate : ''}`}
+        >
+          {heroData.titleHighlights[activeIndex]}
+        </span>
+      </h1>
         <p className={styles.hero__description}>
             {heroData.description}
         </p>
