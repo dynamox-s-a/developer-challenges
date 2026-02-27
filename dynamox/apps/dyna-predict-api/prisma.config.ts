feb175187cfa620a11349e8e46150ef960298936
Eric Reis <ericreis027@gmail.com>
2026-02-27 19:25:18 -0300

    chore: Add prisma config file
    Issue: N/A

---
+++ dynamox/apps/dyna-predict-api/prisma.config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: `.env.${process.env.NODE_ENV ?? 'development'}` });
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
