6e586298a91f1b93607548209677bcb039e99cab
Eric Reis <ericreis027@gmail.com>
2026-03-01 09:32:47 -0300

    feat: Add axios API client
    Issue: N/A

---
+++ dynamox/apps/dyna-predict-web/src/api/client.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import type { createBrowserRouter } from 'react-router-dom';
+
+import type { AppStore } from '../store/store';
+import { logError } from '../utils/logging';
+
+type RemixRouter = ReturnType<typeof createBrowserRouter>;
+
+let store: AppStore;
+let router: RemixRouter;
+
+export const injectStore = (_store: AppStore) => {
+  store = _store;
+};
+
+export const injectRouter = (_router: RemixRouter) => {
+  router = _router;
+};
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === StatusCodes.UNAUTHORIZED) {
+      const isAuthRequest =
+        error.config?.url?.includes('/login') ||
+        error.config?.url?.includes('/logout') ||
+        error.config?.url?.includes('/me');
+
+      if (isAuthRequest) return Promise.reject(error);
+
+      store.dispatch({ type: 'auth/logout/fulfilled' });
+      router.navigate('/auth/login');
+    }
+
+    logError(error, {
+      tags: { section: 'axios-response' },
+      extra: {
+        url: error.config?.url,
+        method: error.config?.method,
+        requestData: error.config?.data,
+        responseData: error.response?.data,
+        status: error.response?.status,
+      },
+    });
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
