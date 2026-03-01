e3cede91a877e95f80af930187cfe6e45fa85752
Eric Reis <ericreis027@gmail.com>
2026-03-01 12:00:09 -0300

    feat: Add contants file in utils
     - Add sensor colors for pie chart
     - Add sensor label for pie chart
    
    Issue: N/A

---
+++ dynamox/apps/dyna-predict-web/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const MACHINE_COLORS = ['#69263c', '#a93d61'];
+export const SENSOR_COLORS = ['#69263c', '#a93d61', '#a47584'];
+export const SENSOR_LABEL_MAP: Record<string, string> = { HFPlus: 'HF+' };
