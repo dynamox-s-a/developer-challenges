da3ab0238b077f328583fdc8e1de0fc4b954914d
Eric Reis <ericreis027@gmail.com>
2026-03-01 09:34:44 -0300

    feat: Add sentry initialization and inject store and routes on project
    Issue: N/A

--- dynamox/apps/dyna-predict-web/src/main.tsx
+++ dynamox/apps/dyna-predict-web/src/main.tsx
@@ -1,18 +1,34 @@
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import { router } from './router';
 import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles/theme'
+import { CssBaseline } from '@mui/material';
+import { injectRouter, injectStore } from './api/client';
+import * as Sentry from '@sentry/react';
 
 // Font Import
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { initSentry } from './utils/sentry';
 
+injectStore(store)
+injectRouter(router)
+
+initSentry()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
+  {
+    onUncaughtError: Sentry.reactErrorHandler(),
+    onCaughtError: Sentry.reactErrorHandler(),
+    onRecoverableError: Sentry.reactErrorHandler(),
+  }
 );
 
 root.render(
