fde4266725d8f92d8735d01d95c57438b2b0bed4
Larissa Schavarski <larissa.cardosolima@outlook.com>
2026-03-02 17:05:44 -0300

    chore: add dockerfile

--- signal-processing-api/db_config.py
+++ signal-processing-api/db_config.py
@@ -1,7 +1,9 @@
+import os
+
 DB_CONFIG = {
-    "host": "localhost",
-    "port": 5433,
-    "database": "signal_processing",
-    "user": "postgres",
-    "password": "postgre"
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5433"),
+    "database": os.getenv("DB_NAME", "signal_processing"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgre")
 }
