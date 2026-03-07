53ac1def564b3efdf94219d72a3a173cdb836995
TecoAdamo <adamomateus478@gmail.com>
2026-03-07 05:22:19 -0300

    fix: add AccentColor to assets

---
+++ DynamoxQuizTests/ViewModels/QuizViewModelTest.swift
@@ -0,0 +1,57 @@
+//
+//  QuizViewModelTests.swift
+//  DynamoxQuiz
+//
+//  Created by Mateus on 06/03/26.
+//
+
+import XCTest
+@testable import DynamoxQuiz
+
+final class QuizViewModelTests: XCTestCase {
+    
+    var sut: QuizViewModel!
+
+    override func setUp(){
+        super.setUp()
+        sut = QuizViewModel()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_quizViewModel_resetQuiz_resetCurrentIndex(){
+        sut.resetQuiz()
+        
+        XCTAssertEqual(sut.currentIndex, 0)
+    }
+    
+    func test_quizViewModel_resetQuiz_resetCorrectAnswerCount(){
+        sut.resetQuiz()
+        
+        XCTAssertEqual(sut.correctAnswerCount, 0)
+    }
+    
+    func test_quizViewModel_resetQuiz_resetCurrentQuestion(){
+        sut.resetQuiz()
+        
+        XCTAssertNil(sut.currentQuestion)
+    }
+    @MainActor
+    func test_quizViewModel_resetQuiz_resetStateEqualQuiz(){
+        sut.resetQuiz()
+        
+        XCTAssertEqual(sut.state, .quiz)
+    }
+
+
+    func test_quizViewModel_nextQuestion_sumNumberQuestion(){
+        let initialIndex = sut.currentIndex
+        
+        sut.nextQuestion()
+        
+        XCTAssertEqual(sut.currentIndex, initialIndex + 1)
+    }
+}
