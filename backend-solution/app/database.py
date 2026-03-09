e4fd2fb2f45b66b43b7335d62d39fd8e5ae229b3
arielfernandes <0.arielfernandes.0@gmail.com>
2026-03-09 02:52:25 -0300

    refactor database connection

--- backend-solution/app/database.py
+++ backend-solution/app/database.py
@@ -1,10 +1,15 @@
+import logging
+import time
 from functools import lru_cache
 
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
 
 from app.models import table_registry
 from app.settings import Settings
+
+logger = logging.getLogger(__name__)
 
 
 @lru_cache
@@ -20,7 +25,27 @@ def get_engine():
 
 def init_db():
     engine = get_engine()
-    table_registry.metadata.create_all(engine)
+    retries = 5
+
+    while retries > 0:
+        try:
+            table_registry.metadata.create_all(engine)
+            logger.info('Database synchronized successfully!')
+            break
+        except OperationalError:
+            retries -= 1
+            logger.warning(
+                f'atabase unavailable. Trying again...'
+                f' ({retries} attempts remaining)'
+            )
+            time.sleep(3)
+
+    if retries == 0:
+        logger.error(
+            'Fatal failure: Could not connect to the'
+            ' database after multiple attempts.'
+        )
+        raise ConnectionError('he database did not respond in time.')
 
 
 def get_session():
