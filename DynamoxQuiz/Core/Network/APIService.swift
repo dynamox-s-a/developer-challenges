9f1c277f1c937792084ae4416a44282d345eae6e
TecoAdamo <adamomateus478@gmail.com>
2026-03-09 00:15:57 -0300

    feat: update api service

--- DynamoxQuiz/Core/Network/APIService.swift
+++ DynamoxQuiz/Core/Network/APIService.swift
@@ -12,8 +12,8 @@ struct APIService: APIServiceDelegate {
     private let baseURL = "https://quiz-api-bwi5hjqyaq-uc.a.run.app"
 
     nonisolated func fetchRandomQuestion() async throws -> Question {
-//        let url = "\(baseURL)/question"
-        let url = String(format: "%@/question", baseURL)
+        let url = "\(baseURL)/question"
+//        let url = String(format: "%@/question", baseURL)
 
         return try await AF.request(url)
             .validate()
