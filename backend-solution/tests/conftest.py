ac8923261560c28a13b2e7fc2cf63adcbe5eb56c
arielfernandes <0.arielfernandes.0@gmail.com>
2026-03-08 23:26:37 -0300

    Test database

--- backend-solution/tests/conftest.py
+++ backend-solution/tests/conftest.py
@@ -2,19 +2,20 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
+from app.database import get_session
 from app.main import app
 from app.models import table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
-
-
-@pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -22,3 +23,16 @@ def session():
 
     table_registry.metadata.drop_all(engine)
     engine.dispose()
+
+
+@pytest.fixture
+def client(session):
+
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
