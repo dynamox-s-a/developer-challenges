4edb159b2eaa1221783b40ae33552bb2f2800a9e
jaovw <joojvw@gmail.com>
2026-03-31 20:50:17 -0300

    feat(infra): setup Swagger UI at /api endpoint

--- signal-processing-api/src/main.ts
+++ signal-processing-api/src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Signal Processing API')
+    .setDescription('Dynamox Back-end Challenge v2 - Time Series & Sensors')
+    .setVersion('1.0')
+    .addTag('time-series')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
