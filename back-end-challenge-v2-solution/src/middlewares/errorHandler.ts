3473b62fcd9088519d5b01757c3a6edbd04e5588
Leonardo Arena <leonardoarena7@gmail.com>
2026-04-03 18:11:30 -0300

    feat: add AppError class and global error handling middleware

---
+++ back-end-challenge-v2-solution/src/middlewares/errorHandler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import AppError from "../errors/AppError.js";
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error("Internal Error:", error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+};
