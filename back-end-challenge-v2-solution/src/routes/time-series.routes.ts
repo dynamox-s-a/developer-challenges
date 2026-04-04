c77d86fb6a9f0e9574a319a8846b441ca0dbdb93
Leonardo Arena <leonardoarena7@gmail.com>
2026-04-04 10:54:31 -0300

    refactor: split request validation into body and params validators

--- back-end-challenge-v2-solution/src/routes/time-series.routes.ts
+++ back-end-challenge-v2-solution/src/routes/time-series.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controllers/time-series.controller.js";
-import { validate } from "../middlewares/validate.js";
+import { validateBody } from "../middlewares/validate.js";
 import { createTimeSeriesSchema } from "../dto/time-series.dto.js";
 
 const router = Router();
 
-router.post("/series", validate(createTimeSeriesSchema), controller.create);
+router.post("/series", validateBody(createTimeSeriesSchema), controller.create);
 router.get("/series/count", controller.count);
 
 export default router;
