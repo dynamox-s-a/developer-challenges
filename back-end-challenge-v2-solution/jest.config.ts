670f43dc05ff4d4a87320511652977613f2615e1
Leonardo Arena <leonardoarena7@gmail.com>
2026-04-05 08:29:21 -0300

    test: silence ts-jest warnings and update Jest transform config

--- back-end-challenge-v2-solution/jest.config.ts
+++ back-end-challenge-v2-solution/jest.config.ts
@@ -1,9 +1,19 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "ts-jest",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        diagnostics: {
+          ignoreCodes: [151002],
+        },
+      },
+    ],
+  },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
