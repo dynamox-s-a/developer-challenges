af0b37deaedc6ad41badd5482b2cf5072a3d29f5
Anderson Lopes <aslopes095@gmail.com>
2026-05-30 20:29:32 -0300

    refactor: remove unused sessionmaker and database connection management

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base
 
 from app.config import settings
 
@@ -7,18 +7,5 @@ DATABASE_URL = settings.DATABASE_URL
 
 engine =  create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-def get_db():
-    db = SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()
 
 Base = declarative_base()
---
+++ app/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import sessionmaker
+
+from app.database import engine
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
