f9cc424111ce924ea88c2ffd80992cf5d4c59d83
Matheus <matheusrochagago@gmail.com>
2026-06-18 18:54:13 -0300

    feat: implementação dos endpoints de sensores

---
+++ backend/api/sensor.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException
+from database import get_db
+from models.sensor import Sensor as SensorModel
+from schemas.sensor import SensorCreate, Sensor
+from service.sensor_service import SensorService
+
+sensor_router=APIRouter(prefix="/sensores", tags=["sensores"])
+
+@sensor_router.post("/", response_model=Sensor)
+async def create_sensor(
+    sensor: SensorCreate, 
+    db: Session = Depends(get_db)
+): 
+    return SensorService.create_sensor(
+        db=db,
+        name=sensor.name
+    )
+
+@sensor_router.get("/")
+async def get_sensors(
+    db: Session = Depends(get_db)
+):
+    return SensorService.get_sensors(db)
+
+@sensor_router.get("/{sensor_id}")
+async def get_sensor(
+    sensor_id: int,
+    db: Session = Depends(get_db)
+):
+    return SensorService.get_sensor(
+        db=db,
+        sensor_id=sensor_id
+    )
+
+@sensor_router.delete("/{sensor_id}")
+async def delete_sensor(
+    sensor_id: int,
+    db: Session = Depends(get_db)
+):
+    return SensorService.delete_sensor(
+        db=db,
+        sensor_id=sensor_id
+    )
