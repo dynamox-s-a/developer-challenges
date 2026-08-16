99ce6f7802846ccad9b1259f1242f82b24734dd7
nicolai <nicolaileo@outlook.com>
2026-08-16 14:30:16 -0300

    feat: serve db data for the App

--- developer-challenges/src/App.tsx
+++ developer-challenges/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { Provider } from "react-redux";
 import "./App.css";
-import { Typography } from "./components/Typography/Typography";
+import { store } from "./app/store";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { fetchMachineData } from "./features/machineData/machineDataSlice";
+
+function MachineDataView() {
+	const dispatch = useAppDispatch();
+	const machineData = useAppSelector((state) => state.machineData);
+
+	useEffect(() => {
+		dispatch(fetchMachineData());
+	}, [dispatch]);
+
+	return <pre>{JSON.stringify(machineData, null, 2)}</pre>;
+}
 
 function App() {
-	return <Typography text="Dynamox test" />;
+	return (
+		<Provider store={store}>
+			<MachineDataView />
+		</Provider>
+	);
 }
 
 export default App;
