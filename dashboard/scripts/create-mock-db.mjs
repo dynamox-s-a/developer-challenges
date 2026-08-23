6592d1724560a7c589623d2f8a24cff4d19e76c3
Alfredo Goeten <alfredo.goeten@outlook.com>
2026-08-23 15:42:55 -0300

    fix mock path

--- dashboard/scripts/create-mock-db.mjs
+++ dashboard/scripts/create-mock-db.mjs
@@ -1,6 +1,6 @@
 import { mkdir, readFile, writeFile } from 'node:fs/promises'
 
-const source = new URL('../mocks/response-challenge-v2.json', import.meta.url)
+const source = new URL('../../response-challenge-v2.json', import.meta.url)
 const targetDirectory = new URL('../.mock/', import.meta.url)
 const target = new URL('./db.json', targetDirectory)
 const data = JSON.parse(await readFile(source, 'utf8'))
